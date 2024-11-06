45f0e798007ad3ff92efa0ef49bceae6e869bff5
Hossein <141348426+Hossein137900@users.noreply.github.com>
2024-11-06 16:53:58 +0330

    Update blogs.tsx

--- src/app/blogs/page.tsx
+++ src/app/blogs/page.tsx
@@ -17,6 +17,9 @@ const handleButtonClick4 = () => {
 const handleButtonClick5 = () => {
   window.location.href = "/how-to-have-a-good-wedding-ceremony";
 };
+const handleButtonClick6 = () => {
+  window.location.href = "/ten-things-to-consider";
+};
 
 const Page = () => {
   useEffect(() => {
@@ -30,7 +33,7 @@ const Page = () => {
     }
   }, []);
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 items-center justify-center gap-4">
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 items-center justify-center gap-4 my-8">
       {/* راهنمای  انتخاب بهترین تالار در تهران */}
 
       <div
@@ -81,7 +84,7 @@ const Page = () => {
           </div>
           <div className="relative text-white px-6 pb-6 mt-6">
             <span className="block opacity-75 -mb-1"></span>
-            <div className="flex justify-between">
+            <div className="flex justify-center">
               <span className="block text-regular ">
                 راهنمای انتخاب بهترین تالار در تهران{" "}
               </span>
@@ -140,7 +143,7 @@ const Page = () => {
           </div>
           <div className="relative text-white px-6 pb-6 mt-6">
             <span className="block opacity-75 -mb-1"></span>
-            <div className="flex justify-between">
+            <div className="flex justify-center">
               <span className="block text-xs ">
                 چطور یک عروسی لاکچری و به‌صرفه داشته باشیم؟
               </span>
@@ -196,7 +199,7 @@ const Page = () => {
           </div>
           <div className="relative text-white px-6 pb-6 mt-6">
             <span className="block opacity-75 -mb-1"></span>
-            <div className="flex justify-between">
+            <div className="flex justify-center">
               <span className="block text-xs ">
                 تالار ورسای : انتخابی عالی برای مراسم‌های فرانسوی
               </span>
@@ -253,7 +256,7 @@ const Page = () => {
           </div>
           <div className="relative text-white px-6 pb-6 mt-6">
             <span className="block opacity-75 -mb-1"></span>
-            <div className="flex justify-between">
+            <div className="flex justify-center">
               <span className="block text-xs ">
                 سفره عقد متفاوت: ایده‌هاای جالب و خاص{" "}
               </span>
@@ -310,9 +313,65 @@ const Page = () => {
           </div>
           <div className="relative text-white px-6 pb-6 mt-6">
             <span className="block opacity-75 -mb-1"></span>
-            <div className="flex justify-between">
+            <div className="flex justify-center">
               <span className="block text-xs ">
                 چگونگی داشتن مراسم عقد به‌یادماندنی و لوکس
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div
+        className="p-1 flex flex-wrap items-center justify-center"
+        onClick={handleButtonClick6}
+      >
+        <div className="flex-shrink-0 m-6 relative overflow-hidden bg-green rounded-lg max-w-xs shadow-lg">
+          <svg
+            className="absolute bottom-0 left-0 mb-8"
+            viewBox="0 0 375 283"
+            fill="none"
+            style={{ transform: "scale(1.5)", opacity: 0.1 }}
+          >
+            <rect
+              x="159.52"
+              y="175"
+              width="152"
+              height="152"
+              rx="8"
+              transform="rotate(-45 159.52 175)"
+              fill="white"
+            />
+            <rect
+              y="107.48"
+              width="152"
+              height="152"
+              rx="8"
+              transform="rotate(-45 0 107.48)"
+              fill="white"
+            />
+          </svg>
+          <div className="relative pt-10 px-10 flex items-center justify-center">
+            <div
+              className="block absolute w-48 h-48 bottom-0 left-0 -mb-24 ml-3"
+              style={{
+                background: "radial-gradient(black, transparent 60%)",
+                transform: "rotate3d(0, 0, 1, 20deg) scale3d(1, 0.6, 1)",
+                opacity: 0.2,
+              }}
+            ></div>
+            <Image
+              className="relative w-40"
+              src="/logo.webp"
+              alt=""
+              width={4000}
+              height={4000}
+            />
+          </div>
+          <div className="relative text-white px-6 pb-6 mt-6">
+            <span className="block opacity-75 -mb-1"></span>
+            <div className="flex justify-center">
+              <span className="block text-xs ">
+                ۱۰ نکته مهم در زمان رزرو تالار عروسی
               </span>
             </div>
           </div>
