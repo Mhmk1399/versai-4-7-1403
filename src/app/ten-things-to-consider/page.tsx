26b56bcf263681017d8917067ebe9321108b5ec1
Hossein <141348426+Hossein137900@users.noreply.github.com>
2024-11-06 16:53:17 +0330

    add new blog

---
+++ src/app/ten-things-to-consider/page.tsx
@@ -0,0 +1,259 @@
+"use client";
+import React, { useEffect } from "react";
+import Image from "next/image";
+import Link from "next/link";
+
+const Page = () => {
+  useEffect(() => {
+    document.title = " ۱۰ نکته مهم در زمان رزرو تالار عروسی";
+    const metaDescription = document.querySelector('meta[name="description"]');
+    if (metaDescription) {
+      metaDescription.setAttribute(
+        "content",
+        " در این مقاله، به ۱۰ نکته کلیدی اشاره می‌کنیم که پیش از رزرو تالار عروسی باید به آنها توجه کنید؛ از ظرفیت و بودجه تا خدمات و دکوراسیون. با رعایت این نکات، می‌توانید تالاری مناسب با سلیقه و نیازهای خود پیدا کنید و مراسمی رویایی و به‌یادماندنی داشته باشید."
+      );
+    }
+  }, []);
+  return (
+    <>
+      <div className="bg-white grid mx-auto grid-cols-1 h-auto p-4 " dir="rtl">
+        <div className="px-4 mx-auto py-2 bg-gray-50 rounded-lg my-2 width-full grid grid-cols-1 gap-4">
+          <div className="col-span-2 mx-auto width-auto my-2 px-1.5 py-1.5 bg-white rounded-lg">
+            <div className="mx-auto width-auto my-2 px-1.5 py-1.5 bg-white rounded-lg">
+              <Image
+                src="/assets/images/photo_2024-11-06_11-07-32.webp"
+                width={4000}
+                height={4000}
+                alt="۱۰ نکته مهم در زمان رزرو تالار عروسی"
+                className="rounded-lg shadow-lg w-full h-full"
+              />
+            </div>
+            <div className="mx-auto width-auto my-2 px-1.5 py-1.5 bg-white rounded-lg text-center">
+              <h1
+                className="text-2xl font-bold mx-auto h1-blog-header"
+                id="contetTitle"
+              >
+                ۱۰ نکته مهم در زمان رزرو تالار عروسی
+              </h1>
+              <p id="contentDiscription" className="text-regular mx-auto mt-2">
+                <span className="h2-blog-header font-bold mb-3">
+                  در زمان برنامه‌ریزی عروسی، انتخاب و رزرو تالار مناسب یکی از
+                  مهم‌ترین بخش‌های مراسم است که می‌تواند تأثیر زیادی بر تجربه
+                  مهمانان و خاطرات زوج‌ها داشته باشد.
+                </span>
+                <br />
+              </p>
+              <p className="text-regular mx-auto my-3" id="contentDiscription">
+                در این مقاله، به ۱۰ نکته کلیدی اشاره می‌کنیم که پیش از رزرو
+                تالار عروسی باید به آنها توجه کنید؛ از ظرفیت و بودجه تا خدمات و
+                دکوراسیون. با رعایت این نکات، می‌توانید تالاری مناسب با سلیقه و
+                نیازهای خود پیدا کنید و مراسمی رویایی و به‌یادماندنی داشته
+                باشید.
+              </p>
+
+              <span className="text-sm font-bold block">
+                در زمان رزرو تالار عروسی، رعایت نکات زیر به شما کمک می‌کند تا
+                انتخاب مناسبی داشته باشید و مراسمی بی‌نقص و به‌یادماندنی برگزار
+                کنید:
+              </span>
+              <Link href="/menue">
+                <button className="bg-gradient-to-l mb-6 from-green to-black text-white hover-btn py-2 px-8 mx-auto rounded-full shadow-lg hover:shadow-2xl transition-transform transform hover:scale-105 mt-8 text-xl">
+                  رزرو سالن عقد شاتو دو ورسای
+                </button>
+              </Link>
+              <div className="mx-auto width-auto my-2 px-1.5 py-1.5 bg-white  rounded-lg text-center">
+                <h2 className="text-1xl font-bold mx-auto h2-blog-header ">
+                  قبل از رزرو تالار عروسی این ۱۰ نکته مهم را بدانید!
+                </h2>
+                <ol
+                  id="contentDiscription"
+                  className="text-regular mx-auto text-gray-700 mt-5 "
+                >
+                  <span className="font-extrabold h-14 bg-gradient-to-r from-amber-500 px-3 py-1  to-Cyan-950 rounded-full">
+                    چگونه تالار عروسی مناسبی رزرو کنیم؟
+                  </span>
+
+                  <li className="text-regular  mt-3 ">
+                    <span className="font-bold mb-3 text-lg ml-2">
+                      1. تعیین بودجه
+                    </span>
+                    <br />
+                    <span>
+                      قبل از شروع به جستجوی تالار، بودجه خود را مشخص کنید. تعیین
+                      هزینه‌های مربوط به اجاره تالار، دکوراسیون، نورپردازی و
+                      خدمات دیگر کمک می‌کند تا از ابتدا در مسیر درستی حرکت کنید.
+                    </span>
+                  </li>
+
+                  <li className="text-regular  mb-3 mt-4">
+                    <span className="font-bold text-lg ml-2 ">
+                      2. بررسی تاریخ‌ها
+                    </span>
+                    <br />
+                    <span>
+                      تاریخ مراسم را با دقت بررسی کنید. بعضی از تالارها در
+                      روزهای خاص (مانند تعطیلات یا آخر هفته‌ها) ممکن است شلوغ‌تر
+                      باشند. برای انتخاب تاریخ بهتر، از قبل با تالارها تماس
+                      بگیرید و از موجودی تاریخ‌ها مطلع شوید.
+                    </span>
+                  </li>
+                  <div className="mx-auto width-auto my-2 px-1.5 py-1.5 bg-white rounded-lg">
+                    <Image
+                      src="/assets/images/date.webp"
+                      width={2000}
+                      height={2000}
+                      alt="۱۰ نکته مهم در زمان رزرو تالار عروسی"
+                      className="rounded-lg shadow-lg w-full h-full lg:max-h-[1000px]"
+                    />
+                  </div>
+                  <li className="text-regular  mb-3">
+                    <span className="font-bold text-lg ml-2">
+                      3. جستجوی محل
+                    </span>
+                    <br />
+                    <span>
+                      مکان تالار یکی از عوامل کلیدی در انتخاب آن است. بررسی کنید
+                      که آیا مکان تالار به آسانی قابل دسترسی است و آیا مهمانان
+                      شما مشکلی برای رسیدن به آنجا خواهند داشت.
+                    </span>
+                  </li>
+                  <div className="mx-auto width-auto my-2 px-1.5 py-1.5 bg-white rounded-lg">
+                    <Image
+                      src="/assets/images/photo_2024-11-06_11-07-40.webp"
+                      width={4000}
+                      height={4000}
+                      alt="چگونه تالار عروسی مناسبی رزرو کنیم"
+                      className="rounded-lg shadow-lg w-full h-full"
+                    />
+                  </div>
+                  <li className="text-regular  mb-3">
+                    <span className="font-bold text-lg ml-2">
+                      4. گنجایش تالار
+                    </span>
+                    <br />
+                    <span>
+                      گنجایش تالار باید با تعداد مهمانان شما هماهنگ باشد.
+                      اطمینان حاصل کنید که فضای کافی برای مهمانان، دکوراسیون و
+                      سایر جزئیات مراسم وجود دارد.
+                    </span>
+                  </li>
+
+                  <li className="text-regular  mb-3">
+                    <span className="font-bold ml-2 text-xl">
+                      5. بررسی امکانات
+                    </span>
+                    <br />
+                    <span>
+                      قبل از رزرو، امکانات تالار را بررسی کنید. آیا سیستم صوتی و
+                      نورپردازی مناسب وجود دارد؟ آیا امکان پارک خودرو برای
+                      مهمانان فراهم است؟ این نکات می‌تواند در تجربه مراسم شما
+                      تأثیرگذار باشد.
+                    </span>
+                    <div className="mx-auto width-auto my-2 px-1.5 py-1.5 bg-white rounded-lg">
+                      <Image
+                        src="/assets/images/emkanat.webp"
+                        width={4000}
+                        height={4000}
+                        alt="جشن عقد باغ تالار"
+                        className="rounded-lg shadow-lg w-full h-full"
+                      />
+                    </div>
+
+                    <Link href="/menue">
+                      <button className="bg-gradient-to-l from-green to-black text-white hover-btn py-2 px-8 mx-auto rounded-full shadow-lg hover:shadow-2xl transition-transform transform hover:scale-105 mt-8 mb-2 text-xl">
+                        مشاهده منو مجموعه شاتو دو ورسای
+                      </button>
+                    </Link>
+                  </li>
+
+                  <li className="text-regular  mb-3">
+                    <span className="font-bold text-lg ml-2">
+                      6. تخفیف‌ها و پیشنهادات ویژه
+                    </span>
+                    <br />
+                    <span>
+                      برخی از تالارها در زمان‌های خاص یا برای رزروهای گروهی
+                      تخفیف‌هایی ارائه می‌دهند. حتماً از این تخفیف‌ها مطلع شوید
+                      و از آنها بهره‌برداری کنید.
+                    </span>
+                  </li>
+
+                  <li className="text-regular  mb-3">
+                    <span className="font-bold text-lg ml-2">
+                      7. سرویس‌دهی و نظافت
+                    </span>
+                    <br />
+                    <span>
+                      اطمینان حاصل کنید که تالار دارای تیم خدماتی حرفه‌ای و منظم
+                      است. بررسی کنید که آیا در طول مراسم، نظافت و خدمات به‌طور
+                      مرتب انجام می‌شود.
+                    </span>
+                  </li>
+
+                  <li className="text-regular  mb-3">
+                    <span className="font-bold text-lg ml-2">
+                      8. گواهی‌ها و مجوزها
+                    </span>
+                    <br />
+                    <span>
+                      قبل از تصمیم‌گیری، از مجوزهای لازم و گواهی‌های تالار مطمئن
+                      شوید. این گواهی‌ها تضمین‌کننده کیفیت و امنیت مراسم شما
+                      هستند.
+                    </span>
+                  </li>
+
+                  <li className="text-regular  mb-3">
+                    <span className="font-bold text-lg ml-2">
+                      9. بررسی نقد و نظرات
+                    </span>
+                    <br />
+                    <span>
+                      نظرات سایر عروس‌ها و دامادها می‌تواند به شما در انتخاب
+                      تالار کمک کند. بررسی نقد و نظرات آنلاین یا مشاوره با
+                      دوستان و آشنایان می‌تواند مفید باشد.
+                    </span>
+                  </li>
+                  <div className="mx-auto width-auto my-2 px-1.5 py-1.5 bg-white rounded-lg">
+                    <Image
+                      src="/assets/images/hall-normal-1.webp"
+                      width={4000}
+                      height={4000}
+                      alt="مبلمان راحت و لوکس: صندلی‌های زیبا و راحت"
+                      className="rounded-lg shadow-lg w-full h-full"
+                    />
+                  </div>
+                  <li className="text-regular  mb-3">
+                    <span className="font-bold text-lg ml-2">
+                      10. برقراری ارتباط با مدیریت
+                    </span>
+                    <br />
+                    <span>
+                      قبل از نهایی کردن رزرو، حتماً با مدیریت تالار صحبت کنید و
+                      سوالات خود را مطرح کنید. ارتباط خوب با مدیریت می‌تواند به
+                      شما در برگزاری یک مراسم بی‌نقص کمک کند.
+                    </span>
+                  </li>
+
+                  <span className="text-lg">
+                    با رعایت این نکات، می‌توانید تجربه‌ای فراموش‌نشدنی و بی‌نقص
+                    برای مراسم عروسی خود رقم بزنید. برای رزرو تالار و اطلاعات
+                    بیشتر، همین حالا با ما تماس بگیرید و لحظات شیرین خود را آغاز
+                    کنید!
+                  </span>
+                </ol>
+
+                <Link href="https://tour.panoee.net/Versay/65df91a47a73f2e85a4da72b">
+                  <button className="bg-gradient-to-l from-green to-black text-white hover-btn py-2 px-8 mx-auto rounded-full shadow-lg hover:shadow-2xl transition-transform transform hover:scale-105 mt-8 mb-2 text-xl">
+                    میتوانید برای دیدن فضای سه بعدی مجموعه کلیک کنید.
+                  </button>
+                </Link>
+              </div>
+              <hr />
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+export default Page;
