8fadc1c3a03516400ad77d01e3fc4d6e6184bb6e
Hossein <141348426+Hossein137900@users.noreply.github.com>
2024-11-09 15:27:01 +0330

    Add files via upload

---
+++ src/app/Types-of-halls/page.tsx
@@ -0,0 +1,245 @@
+"use client";
+import React, { useEffect } from "react";
+import Image from "next/image";
+import Link from "next/link";
+
+const Page = () => {
+  useEffect(() => {
+    document.title = "انواع تالار و باغ تالار برای انتخاب در فصل‌های مختلف سال";
+    const metaDescription = document.querySelector('meta[name="description"]');
+    if (metaDescription) {
+      metaDescription.setAttribute(
+        "content",
+        "با انواع تالار و باغ تالار مناسب برای هر فصل آشنا شوید! بهترین انتخاب‌ها برای برگزاری مراسم در فصول مختلف سال با توجه به ویژگی‌های آب و هوایی و زیبایی‌های طبیعی"
+      );
+    }
+  }, []);
+  return (
+    <>
+      <div className="bg-white grid mx-auto grid-cols-1 h-auto p-4 " dir="rtl">
+        <div className="px-4 mx-auto py-2 bg-gray-50 rounded-lg my-2 width-full grid grid-cols-1 gap-4">
+          <div className="col-span-2 mx-auto width-auto my-2 px-1.5 py-1.5 bg-white rounded-lg">
+            <div className="mx-auto width-auto my-2 px-1.5 py-1.5 bg-white rounded-lg">
+              <Image
+                src="/assets/images/hall-normal-1.webp"
+                width={4000}
+                height={4000}
+                alt="انواع تالار و باغ تالار مناسب برای هر فصل"
+                className="rounded-lg shadow-lg w-full h-full"
+              />
+            </div>
+            <div className="mx-auto width-auto my-2 px-1.5 py-1.5 bg-white rounded-lg text-center">
+              <h1
+                className="text-2xl font-bold mx-auto h1-blog-header"
+                id="contetTitle"
+              >
+                انواع تالار و باغ تالار برای انتخاب در فصل‌های مختلف سال{" "}
+              </h1>
+              <p id="contentDiscription" className="text-regular mx-auto mt-2">
+                <span className="h2-blog-header font-bold mb-3">
+                  برگزاری مراسم در فصول مختلف سال نیازمند انتخاب مکان مناسب است
+                  تا تجربه‌ای به یادماندنی برای شما و مهمانانتان ایجاد شود. هر
+                  فصل با ویژگی‌های آب و هوایی و زیبایی‌های خاص خود می‌تواند
+                  تأثیر زیادی بر نوع تالار یا باغ تالاری که انتخاب می‌کنید داشته
+                  باشد.
+                </span>
+                <br />
+              </p>
+              <p className="text-regular mx-auto my-3" id="contentDiscription">
+                در این مطلب، انواع تالار و باغ تالارهایی که برای فصل‌های مختلف
+                مناسب هستند را معرفی می‌کنیم و به نکاتی برای انتخاب بهترین گزینه
+                می‌پردازیم.
+              </p>
+
+              <span className="text-sm font-bold block">
+                برگزاری مراسم در محیطی هماهنگ با فصل، علاوه بر زیبایی بصری،
+                راحتی و آسایش بیشتری برای مهمانان فراهم می‌کند. برای مثال، در
+                فصل بهار و تابستان که هوا معتدل و طبیعت سرسبز است، باغ تالارها
+                گزینه‌ای ایده‌آل به شمار می‌آیند؛ اما در پاییز و زمستان با سرد
+                شدن هوا، تالارهای سرپوشیده با سیستم‌های گرمایشی مناسب، انتخاب
+                بهتری خواهند بود. همچنین، انتخاب نوع تالار یا باغ تالار می‌تواند
+                به سبک و تم مراسم شما جلوه بیشتری بدهد و تأثیر زیادی بر کیفیت
+                عکس‌ها و فیلم‌های مراسم بگذارد. در ادامه به بررسی ویژگی‌های هر
+                نوع تالار و باغ تالار در فصول مختلف سال می‌پردازیم تا بتوانید با
+                توجه به فصل، بهترین تصمیم را برای روز خاص خود بگیرید.
+              </span>
+              <Link href="/menue">
+                <button className="bg-gradient-to-l mb-6 from-green to-black text-white hover-btn py-2 px-8 mx-auto rounded-full shadow-lg hover:shadow-2xl transition-transform transform hover:scale-105 mt-8 text-xl">
+                  رزرو سالن عقد شاتو دو ورسای
+                </button>
+              </Link>
+              <div className="mx-auto width-auto my-2 px-1.5 py-1.5 bg-white  rounded-lg text-center">
+                <h2 className="text-1xl font-bold mx-auto h2-blog-header ">
+                  راهنمای کامل انتخاب تالار و باغ تالار بر اساس فصل{" "}
+                </h2>
+                <ol
+                  id="contentDiscription"
+                  className="text-regular mx-auto text-gray-700 mt-5 "
+                >
+                  <span className="font-extrabold h-14 bg-gradient-to-r from-amber-500 px-3 py-1  to-Cyan-950 rounded-full">
+                    نکاتی برای برگزاری مراسم بی‌نقص{" "}
+                  </span>
+
+                  <li className="text-regular  mt-3 ">
+                    <span className="font-bold mb-3 text-lg ml-2">
+                      ۱. تالارهای بسته در زمستان
+                    </span>
+                    <br />
+                    <span>
+                      در فصل زمستان، تالارهای بسته بهترین گزینه برای برگزاری
+                      مراسم عروسی هستند. این تالارها به شما این امکان را می‌دهند
+                      که از سرما و بارش برف در امان باشید. با دکوراسیون‌های گرم
+                      و مناسب، می‌توانید فضایی رمانتیک و دلپذیر ایجاد کنید.
+                      همچنین، نورپردازی گرم و استفاده از شمع‌ها می‌تواند فضای
+                      زیبایی را به وجود آورد. به یاد داشته باشید که می‌توانید از
+                      تزئینات زمستانی مانند درختان کریسمس یا گل‌های طبیعی نیز
+                      استفاده کنید تا فضای تالار شما را شگفت‌انگیزتر کند.
+                    </span>
+                  </li>
+                  <div className="mx-auto flex justify-center items-center my-6 px-1.5 py-1.5 bg-white rounded-lg">
+                    <Image
+                      src="/assets/images/winter.webp"
+                      width={1000}
+                      height={4000}
+                      alt="تالارهای بسته در زمستان"
+                      className="rounded-lg shadow-lg lg:max-h-[900px] mx-auto"
+                    />
+                  </div>
+
+                  <li className="text-regular  mb-3 mt-4">
+                    <span className="font-bold text-lg ml-2 ">
+                      ۲. باغ تالارها در بهار
+                    </span>
+                    <br />
+                    <span>
+                      بهار فصل شکوفایی و زیبایی طبیعت است و باغ تالارها
+                      می‌توانند انتخاب مناسبی برای این فصل باشند. با گل‌های تازه
+                      و درختان سرسبز، فضای باغ تالار شما می‌تواند بسیار زیبا و
+                      رمانتیک باشد. همچنین، برگزاری مراسم در فضای باز به شما این
+                      امکان را می‌دهد که از هوای مطبوع بهاری بهره‌مند شوید.
+                      استفاده از دکوراسیون‌های الهام‌گرفته از طبیعت، مانند
+                      گلدان‌های گل و تزئینات سبز، می‌تواند این فضا را زیباتر
+                      کند.
+                    </span>
+                  </li>
+                  <div className="mx-auto flex justify-center items-center my-6 px-1.5 py-1.5 bg-white rounded-lg">
+                    <Image
+                      src="/assets/images/spring.webp"
+                      width={1000}
+                      height={4000}
+                      alt="باغ تالارها در بهار"
+                      className="rounded-lg shadow-lg lg:max-h-[900px] mx-auto"
+                    />
+                  </div>
+                  <li className="text-regular  mb-3">
+                    <span className="font-bold text-lg ml-2">
+                      ۳. تالارهای پذیرایی در تابستان
+                    </span>
+                    <br />
+                    <span>
+                      فصل تابستان به دلیل گرما، تالارهای بزرگ با تهویه مطبوع
+                      گزینه‌های مناسبی هستند. در این فصل، می‌توانید با استفاده
+                      از دکوراسیون‌های خنک و رنگ‌های روشن، فضایی شاداب و پر
+                      انرژی ایجاد کنید. همچنین، اگر به دنبال فضایی خاص هستید،
+                      می‌توانید از باغ تالارهای دارای سایبان استفاده کنید تا
+                      مهمانان شما در سایه از هوای تابستان لذت ببرند. توجه به
+                      جزئیات مانند آب‌نماها و تجهیزات خنک‌کننده نیز می‌تواند
+                      تجربه مهمانان را بهبود بخشد.
+                    </span>
+                  </li>
+                  <div className="mx-auto flex justify-center items-center my-6 px-1.5 py-1.5 bg-white rounded-lg">
+                    <Image
+                      src="/assets/images/summer.webp"
+                      width={1000}
+                      height={4000}
+                      alt="تالارهای پذیرایی در تابستان"
+                      className="rounded-lg shadow-lg lg:max-h-[900px] mx-auto"
+                    />
+                  </div>
+                  <li className="text-regular  mb-3">
+                    <span className="font-bold text-lg ml-2">
+                      ۴. باغ تالارها در پاییز
+                    </span>
+                    <br />
+                    <span>
+                      پاییز با رنگ‌های گرم و دلنشینش زمان مناسبی برای برگزاری
+                      مراسم در باغ تالارها است. با تغییر رنگ برگ‌ها و هوای
+                      ملایم، باغ تالار شما می‌تواند فضایی خاص و منحصر به فرد
+                      ایجاد کند. استفاده از رنگ‌های پاییزی در دکوراسیون، مانند
+                      زرد، نارنجی و قرمز، می‌تواند زیبایی مراسم شما را دوچندان
+                      کند. همچنین، شما می‌توانید از دکوراسیون‌های خلاقانه‌ای مثل
+                      کدو تنبل و چایخانه‌های سنتی برای تزئین فضای مراسم استفاده
+                      کنید.
+                    </span>
+                  </li>
+                  <div className="mx-auto flex justify-center items-center my-6 px-1.5 py-1.5 bg-white rounded-lg">
+                    <Image
+                      src="/assets/images/autumn.webp"
+                      width={1000}
+                      height={4000}
+                      alt=" باغ تالارها در پاییز"
+                      className="rounded-lg shadow-lg lg:max-h-[900px] mx-auto"
+                    />
+                  </div>
+
+                  <li className="text-regular  mb-3">
+                    <span className="font-bold text-lg ml-2">
+                      ۵. نکات کلی برای انتخاب تالار یا باغ تالار
+                    </span>
+                    <br />
+                    <span>
+                      <strong>موقعیت جغرافیایی: </strong>
+                    </span>
+                    <span>
+                      به راحتی دسترسی مهمانان و شرایط جوی هر فصل توجه کنید.
+                      انتخاب تالاری که به مناطق اصلی شهر نزدیک است می‌تواند برای
+                      مهمانان راحت‌تر باشد.
+                    </span>
+                    <br />
+                    <br />
+                    <span>
+                      <strong>تعداد مهمانان: </strong>
+                    </span>
+                    <span>
+                      به راحتی دسترسی مهمانان و شرایط جوی هر فصل توجه کنید.
+                      انتخاب تالاری که به مناطق اصلی شهر نزدیک است می‌تواند برای
+                      مهمانان راحت‌تر باشد.
+                    </span>
+                    <br />
+                    <br />
+                    <span>
+                      <strong>دکوراسیون و امکانات:</strong>
+                    </span>
+                    <span>
+                      بررسی کنید که آیا تالار دارای دکوراسیون مناسب برای هر فصل
+                      است و آیا امکانات کافی را ارائه می‌دهد. همچنین، امکاناتی
+                      مانند پارکینگ، اینترنت و سیستم صوتی نیز از اهمیت ویژه‌ای
+                      برخوردارند.
+                    </span>
+                  </li>
+
+                  <span className="text-lg">
+                    با انتخاب مناسب تالار یا باغ تالار با توجه به فصل، می‌توانید
+                    لحظاتی فراموش‌نشدنی برای مراسم عروسی خود رقم بزنید. از
+                    جزئیات کوچک مانند نوع گل‌ها و دکوراسیون گرفته تا بزرگ‌ترین
+                    عناصر، همه چیز می‌تواند در برگزاری یک جشن بی‌نظیر تأثیرگذار
+                    باشد. برای کسب اطلاعات بیشتر و رزرو، همین حالا با ما تماس
+                    بگیرید و جشن عروسی خود را آغاز کنید!
+                  </span>
+                </ol>
+
+                <Link href="https://tour.panoee.net/Versay/65df91a47a73f2e85a4da72b">
+                  <button className="bg-gradient-to-l from-green to-black text-white hover-btn py-2 px-8 mx-auto rounded-full shadow-lg hover:shadow-2xl transition-transform transform hover:scale-105 mt-8 mb-2 text-xl">
+                    میتوانید برای دیدن فضای سه بعدی مجموعه کلیک کنید.
+                  </button>
+                </Link>
+              </div>
+              <hr />
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+export default Page;
