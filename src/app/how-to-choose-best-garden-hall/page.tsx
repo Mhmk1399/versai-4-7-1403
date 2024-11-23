8c0cc01aa649288ddd91544616d917b0c75c7722
Hossein <141348426+Hossein137900@users.noreply.github.com>
2024-11-23 11:40:09 +0330

    Add files via upload

---
+++ src/app/how-to-choose-best-garden-hall/page.tsx
@@ -0,0 +1,291 @@
+"use client";
+import React, { useEffect } from "react";
+import Image from "next/image";
+import Link from "next/link";
+
+const Page = () => {
+  useEffect(() => {
+    document.title = "چگونه بهترین باغ تالار را برای عروسی خود انتخاب کنیم؟";
+    const metaDescription = document.querySelector('meta[name="description"]');
+    if (metaDescription) {
+      metaDescription.setAttribute(
+        "content",
+        "در این مقاله، نکات کلیدی و پیشنهادهایی را بررسی می‌کنیم تا به شما کمک کنیم باغ تالار ایده‌آل خود را پیدا کنید و مراسمی بی‌نقص برگزار کنید."
+      );
+    }
+  }, []);
+  return (
+    <>
+      <div className="bg-white grid mx-auto grid-cols-1 h-auto p-4 " dir="rtl">
+        <div className="px-4 mx-auto py-2 bg-gray-50 rounded-lg my-2 width-full grid grid-cols-1 gap-4">
+          <div className="col-span-2 mx-auto width-auto my-2 px-1.5 py-1.5 bg-white rounded-lg">
+            <div className="mx-auto flex flex-col justify-center items-center my-2 px-1.5 py-1.5 bg-white rounded-lg">
+              <Image
+                src="/assets/images/blog12hero.webp"
+                width={4000}
+                height={4000}
+                alt="چگونه بهترین باغ تالار را برای عروسی خود انتخاب کنیم؟"
+                className="rounded-lg shadow-lg h-full"
+              />
+            </div>
+            <div className="mx-auto width-auto my-2 px-1.5 py-1.5 bg-white rounded-lg text-center">
+              <h1
+                className="text-2xl font-bold mx-auto h1-blog-header"
+                id="contetTitle"
+              >
+                راهنمای طلایی برای انتخاب بهترین باغ تالار عروسی
+              </h1>
+              <p id="contentDiscription" className="text-regular mx-auto mt-2">
+                <span className="h2-blog-header font-bold mb-3 text-2xl">
+                  مقدمه
+                </span>
+                <br />
+              </p>
+              <p className="p-2">
+                وقتی که به انتخاب محل برگزاری عروسی فکر می‌کنیم، باغ تالار یکی
+                از بهترین گزینه‌ها است که می‌تواند شبی رؤیایی را برای شما و
+                مهمانان‌تان رقم بزند. اما انتخاب یک باغ تالار مناسب نیاز به
+                بررسی و دقت دارد. در این مقاله می‌خواهیم با استفاده از تجربه‌ها
+                و نکاتی که مجموعه تشریفاتی شاتو دو ورسای به مشتریان خود ارائه
+                می‌دهد، شما را در انتخاب بهترین مکان برای عروسی‌تان همراهی کنیم.
+              </p>
+
+              <Link href="/menue">
+                <button className="bg-gradient-to-l mb-6 from-green to-black text-white hover-btn py-2 px-8 mx-auto rounded-full shadow-lg hover:shadow-2xl transition-transform transform hover:scale-105 mt-8 text-xl">
+                  رزرو سالن عقد شاتو دو ورسای
+                </button>
+              </Link>
+              <div className="mx-auto width-auto my-2 px-1.5 py-1.5 bg-white  rounded-lg text-center">
+                <h2 className="text-1xl font-bold mx-auto h2-blog-header ">
+                  چگونه باغ تالار مناسب برای عروسی خود را بدون استرس پیدا کنیم؟
+                </h2>
+                <ol
+                  id="contentDiscription"
+                  className="text-regular mx-auto text-gray-700 mt-5 "
+                >
+                  <span className="font-extrabold h-14 bg-gradient-to-r from-amber-500 px-3 py-1  to-Cyan-950 rounded-full">
+                    چطور باغ تالار ایده‌آل عروسیتان را پیدا کنید؟
+                  </span>
+
+                  <li className="text-regular  mt-3 ">
+                    <span className="font-bold mb-3 text-lg ml-2">
+                      ۱. ویژگی‌های یک باغ تالار ایده‌آل چیست؟
+                    </span>
+                    <br />
+                    <span className="p-2">
+                      اولین و مهم‌ترین قدم در انتخاب باغ تالار، بررسی ویژگی‌های
+                      مختلف آن است. عواملی مانند فضای سبز زیبا، امکانات کامل، و
+                      قابلیت برگزاری مراسم در فضاهای مختلف از جمله نکاتی است که
+                      باید به آن‌ها توجه داشته باشید. مجموعه شاتو دو ورسای با
+                      محیطی منحصربه‌فرد و طراحی‌های مدرن، همه این موارد را در
+                      اختیار شما قرار می‌دهد.
+                    </span>
+                    <p className="font-bold my-3 text-lg ml-2 text-gold">
+                      نکات مهم:
+                    </p>
+                    <span>1 - فضای بزرگ و دلنشین</span>
+                    <br />
+                    <span>2 - طراحی و دکوراسیون لوکس</span>
+                    <br />
+                    <span>3 - خدمات حرفه‌ای تشریفاتی</span>
+                    <br />
+                    <span>4 - موقعیت جغرافیایی مناسب</span>
+                  </li>
+                  <div className="mx-auto flex flex-col items-center my-2 px-1.5 py-1.5 bg-white rounded-lg">
+                    <Image
+                      src="/assets/images/blog12.webp"
+                      width={2000}
+                      height={2000}
+                      alt="بهترین باغ تالار را برای عروسی"
+                      className="rounded-lg shadow-lg lg:max-h-[900px] mx-auto"
+                    />
+                  </div>
+
+                  <li className="text-regular  mb-3 mt-4">
+                    <span className="font-bold text-lg ml-2 ">
+                      ۲. نکاتی که باید هنگام رزرو باغ تالار در نظر بگیرید
+                    </span>
+                    <br />
+                    <span className="p-2">
+                      هنگامی که تصمیم به رزرو باغ تالار دارید، باید برخی جزئیات
+                      را در نظر بگیرید. این جزئیات می‌تواند شامل مواردی مانند
+                      زمان مناسب برگزاری مراسم، تعداد مهمانان، و البته بودجه‌ای
+                      که برای این امر در نظر گرفته‌اید، باشد. شاتو دو ورسای
+                      تمامی این امکانات را با توجه به نیازهای شما فراهم می‌کند.
+                    </span>
+                    <p className="font-bold my-3 text-lg ml-2 text-gold">
+                      نکات قابل توجه:
+                    </p>
+                    <span>1 - زمان‌بندی مناسب برای رزرو</span>
+                    <br />
+                    <span>2 - بررسی ظرفیت تالار برای تعداد مهمانان</span>
+                    <br />
+                    <span>3 - بودجه‌بندی بر اساس خدمات ارائه شده</span>
+                  </li>
+                  <div className="mx-auto flex flex-col items-center my-2 px-1.5 py-1.5 bg-white rounded-lg">
+                    <Image
+                      src="/assets/images/bloog12.webp"
+                      width={2000}
+                      height={2000}
+                      alt="بهترین باغ تالار را برای عروسی"
+                      className="rounded-lg shadow-lg lg:max-h-[900px] mx-auto"
+                    />
+                  </div>
+
+                  <li className="text-regular  mb-3">
+                    <span className="font-bold text-lg ml-2">
+                      ۳. چرا شاتو دو ورسای انتخاب مناسبی برای عروسی شماست؟
+                    </span>
+                    <br />
+                    <span className="p-2">
+                      باغ تالار شاتو دو ورسای با ارائه امکانات ویژه و خدماتی
+                      بی‌نظیر، یکی از بهترین گزینه‌ها برای برگزاری مراسم عروسی
+                      است. از دیزاین شیک و مدرن گرفته تا فضای باز دلنشین، این
+                      مکان هر آنچه که برای یک عروسی باشکوه نیاز دارید را در
+                      اختیار شما می‌گذارد.
+                    </span>
+                    <p className="font-bold my-3 text-lg ml-2 text-gold">
+                      امکانات ویژه:
+                    </p>
+                    <span>1 - نورپردازی حرفه‌ای و مدرن</span>
+                    <br />
+                    <span>2 - سالن‌های بزرگ و مجهز</span>
+                    <br />
+                    <span>3 - تیم تشریفاتی حرفه‌ای</span>
+                  </li>
+                  <div className="mx-auto flex flex-col items-center my-2 px-1.5 py-1.5 bg-white rounded-lg">
+                    <Image
+                      src="/assets/images/Self-service-fish-soup-in-the-international-menu-of-the-Chateau-de-Versailles-luxury-hall-garden.webp"
+                      width={4000}
+                      height={4000}
+                      alt=" ویژگی‌های امکانات رفاهی:"
+                      className="rounded-lg shadow-lg lg:max-h-[900px] mx-auto"
+                    />
+                  </div>
+                  <li className="text-regular  mb-3">
+                    <span className="font-bold text-lg ml-2">
+                      ۴. چگونه مراسم خود را در شاتو دو ورسای رویایی کنید؟
+                    </span>
+                    <br />
+                    <span className="p-2">
+                      هر مراسم عروسی باید خاص و منحصر به فرد باشد و شاتو دو
+                      ورسای این امکان را به شما می‌دهد تا جزییات مراسم‌تان را بر
+                      اساس سلیقه شخصی خودتان طراحی کنید. از انتخاب گل‌آرایی
+                      گرفته تا نحوه چیدمان میزها، همه چیز بر اساس خواست شما
+                      انجام می‌شود.
+                    </span>
+                    <p className="font-bold my-3 text-lg ml-2 text-gold">
+                      پیشنهادات:
+                    </p>
+                    <span>1 - انتخاب گل‌های تازه و زیبا برای تزیین</span>
+                    <br />
+                    <span>2 - طراحی میز شام و تزیینات داخلی</span>
+                    <br />
+                    <span>3 - استفاده از تم‌های مختلف برای مراسم</span>
+                  </li>
+                  <div className="mx-auto flex flex-col items-center my-2 px-1.5 py-1.5 bg-white rounded-lg">
+                    <Image
+                      src="/assets/images/food-normal-services.webp"
+                      width={4000}
+                      height={4000}
+                      alt="  ویژگی‌های امکانات رفاهی"
+                      className="rounded-lg shadow-lg lg:max-h-[900px] mx-auto"
+                    />
+                  </div>
+
+                  <li className="text-regular  mb-3">
+                    <p className="font-bold ml-2 text-xl">
+                      ۵. مراسم عروسی در فضای باز: تجربه‌ای متفاوت
+                    </p>
+                    <span className="p-2">
+                      اگر به برگزاری مراسم در فضای باز علاقه‌مند هستید، شاتو دو
+                      ورسای با فضای باز وسیع و دلنشین خود می‌تواند انتخابی
+                      بی‌نظیر برای شما باشد. برگزاری مراسم در فضای باز نه تنها
+                      جلوه‌ای ویژه به عروسی شما می‌بخشد، بلکه لحظات خاطره‌انگیزی
+                      را برای شما و مهمانان‌تان رقم خواهد زد.
+                    </span>
+                    <p className="font-bold my-3 text-lg ml-2 text-gold">
+                      مزایای فضای باز:
+                    </p>
+                    <span>1 - عکاسی زیبا در طبیعت</span>
+                    <br />
+                    <span>2 - فضای مناسب برای برگزاری مراسم‌های بزرگ</span>
+                    <br />
+                    <span>3 - استفاده از نور طبیعی برای خلق لحظات ماندگار</span>
+                    <br />
+                    <Link href="/menue">
+                      <button className="bg-gradient-to-l from-green to-black text-white hover-btn py-2 px-8 mx-auto rounded-full shadow-lg hover:shadow-2xl transition-transform transform hover:scale-105 mt-8 mb-2 text-xl">
+                        مشاهده منو مجموعه شاتو دو ورسای
+                      </button>
+                    </Link>
+                  </li>
+                  <div className="mx-auto flex flex-col items-center my-2 px-1.5 py-1.5 bg-white rounded-lg">
+                    <Image
+                      src="/assets/images/VAG_2040.webp"
+                      width={4000}
+                      height={4000}
+                      alt=" ویژگی‌های امکانات رفاهی"
+                      className="rounded-lg shadow-lg lg:max-h-[900px] mx-auto"
+                    />
+                  </div>
+                  <li className="text-regular  mb-3">
+                    <p className="font-bold ml-2 text-xl">
+                      ۶. تجربه مهمانان: نظر عروس‌ها درباره شاتو دو ورسای
+                    </p>
+                    <span className="p-2">
+                      یکی از بهترین راه‌ها برای اطمینان از کیفیت یک باغ تالار،
+                      مطالعه نظرات و تجربیات دیگر عروس‌ها است. بسیاری از
+                      عروس‌هایی که مراسم‌شان را در شاتو دو ورسای برگزار
+                      کرده‌اند، تجربه‌های خود را به اشتراک گذاشته‌اند و از خدمات
+                      بی‌نظیر این مجموعه تعریف کرده‌اند.
+                    </span>
+                    <p className="font-bold my-3 text-lg ml-2 text-gold">
+                      نظرات عروس‌ها:
+                    </p>
+                    <span>1 - همه چیز بی‌نقص بود، از تزیینات تا خدمات!</span>
+                    <br />
+                    <span>
+                      2 - مهمانان ما از فضای شیک و لوکس تالار شگفت‌زده شدند.
+                    </span>
+                    <br />
+                    <span>
+                      3 - فضای باز تالار و گل‌آرایی‌ها فوق‌العاده بود.
+                    </span>
+                  </li>
+                  <div className="mx-auto flex flex-col items-center my-2 px-1.5 py-1.5 bg-white rounded-lg">
+                    <Image
+                      src="/assets/images/khadamat3.webp"
+                      width={4000}
+                      height={4000}
+                      alt="ویژگی‌های موقعیت مکانی"
+                      className="rounded-lg shadow-lg lg:max-h-[900px] mx-auto"
+                    />
+                  </div>
+                </ol>
+                <h3 className="font-bold text-2xl my-2">نتیجه‌گیری</h3>
+
+                <span className="p-2">
+                  انتخاب باغ تالار مناسب برای عروسی، یکی از مهم‌ترین تصمیماتی
+                  است که برای روز خاص خود می‌گیرید. با انتخاب شاتو دو ورسای، نه
+                  تنها فضایی زیبا و شیک را تجربه خواهید کرد، بلکه خدماتی حرفه‌ای
+                  و با کیفیت را برای برگزاری مراسمی به‌یادماندنی دریافت خواهید
+                  کرد. پس اگر به دنبال یک تجربه خاص و لوکس هستید، شاتو دو ورسای
+                  انتخابی بی‌نظیر برای شماست.
+                </span>
+                <br />
+
+                <Link href="https://tour.panoee.net/Versay/65df91a47a73f2e85a4da72b">
+                  <button className="bg-gradient-to-l from-green to-black text-white hover-btn py-2 px-8 mx-auto rounded-full shadow-lg hover:shadow-2xl transition-transform transform hover:scale-105 mt-8 mb-2 text-xl">
+                    میتوانید برای دیدن فضای سه بعدی مجموعه کلیک کنید.
+                  </button>
+                </Link>
+              </div>
+              <hr />
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+export default Page;
