87c898ff670267cdd54e62ed0bde2eae4b8094ec
Hossein <141348426+Hossein137900@users.noreply.github.com>
2024-12-14 13:09:42 +0330

    Add files via upload

---
+++ src/app/wedding-table-flowers/page.tsx
@@ -0,0 +1,352 @@
+"use client";
+import React, { useEffect } from "react";
+import Image from "next/image";
+import Link from "next/link";
+
+const Page = () => {
+  useEffect(() => {
+    document.title = "  هماهنگی گل های سفره عقد با تم عروسی  ";
+    const metaDescription = document.querySelector('meta[name="description"]');
+    if (metaDescription) {
+      metaDescription.setAttribute(
+        "content",
+        "آیا برای انتخاب گل‌های سفره عقد و هماهنگی آن‌ها با تم عروسی سردرگم هستید؟ این راهنما به شما کمک می‌کند تا بدون استرس، زیباترین ترکیب را برای مراسم خود بسازید."
+      );
+    }
+  }, []);
+  return (
+    <>
+      <div className="bg-white grid mx-auto grid-cols-1 h-auto p-4 " dir="rtl">
+        <div className="px-4 mx-auto py-2 bg-gray-50 rounded-lg my-2 width-full grid grid-cols-1 gap-4">
+          <div className="col-span-2 mx-auto width-auto my-2 px-1.5 py-1.5 bg-white rounded-lg">
+            <div className="mx-auto flex flex-col justify-center items-center my-2 px-1.5 py-1.5 bg-white rounded-lg">
+              <Image
+                src="/assets/images/201.webp"
+                width={4000}
+                height={4000}
+                alt="  عقد و عروسی"
+                className="rounded-lg shadow-lg h-full"
+              />
+            </div>
+            <div className="mx-auto width-auto my-2 px-1.5 py-1.5 bg-white rounded-lg text-center">
+              <h1
+                className="text-2xl font-bold mx-auto h1-blog-header"
+                id="contetTitle"
+              >
+                چگونه گل‌های سفره عقد را با تم عروسی هماهنگ کنیم؟
+              </h1>
+
+              <p className="p-2">
+                گل‌ها یکی از مهم‌ترین عناصر در سفره عقد هستند که علاوه بر
+                زیبایی، حس شادابی و زندگی را به مراسم می‌آورند. انتخاب درست
+                گل‌ها و هماهنگی آن‌ها با تم عروسی می‌تواند تأثیر بسیاری بر فضای
+                کلی مراسم داشته باشد. در این مقاله، راهکارهایی برای انتخاب و
+                تزیین گل‌های سفره عقد متناسب با تم عروسی بررسی می‌کنیم.
+              </p>
+
+              <Link href="/menue">
+                <button className="bg-gradient-to-l mb-6 from-green to-black text-white hover-btn py-2 px-8 mx-auto rounded-full shadow-lg hover:shadow-2xl transition-transform transform hover:scale-105 mt-8 text-xl">
+                  رزرو سالن عقد شاتو دو ورسای
+                </button>
+              </Link>
+              <div className="mx-auto width-auto my-2 px-1.5 py-1.5 bg-white  rounded-lg text-center">
+                <h2 className="text-1xl font-bold mx-auto h2-blog-header ">
+                  ترفندهای شگفت‌انگیز برای هماهنگی گل‌های سفره عقد و تم عروسی
+                </h2>
+                <ol
+                  id="contentDiscription"
+                  className="text-regular mx-auto text-gray-700 mt-5 "
+                >
+                  <span className="font-extrabold h-14 bg-gradient-to-r from-amber-500 px-3 py-1  to-Cyan-950 rounded-full">
+                    راز هماهنگی گل‌های سفره عقد با تم عروسی!
+                  </span>
+
+                  <li className="text-regular  mt-3 ">
+                    <span className="font-bold mb-3 text-lg ml-2">
+                      ۱. انتخاب رنگ گل‌ها بر اساس تم عروسی
+                    </span>
+                    <br />
+                    <span className="p-2">
+                      یکی از اولین نکات در انتخاب گل‌های سفره عقد، هماهنگی آن‌ها
+                      با رنگ تم عروسی است. رنگ گل‌ها باید به‌گونه‌ای انتخاب شوند
+                      که با دکوراسیون کلی سالن، لباس عروس و دیگر عناصر تزئینی
+                      همخوانی داشته باشند.
+                    </span>
+                    <br />
+                    <span className="font-bold text-gold">
+                      1 - رنگ‌های کلاسیک و شیک:
+                    </span>
+                    <p>
+                      اگر تم عروسی شما سفید و طلایی است، می‌توانید از گل‌های
+                      سفید مانند رز، لیلیوم و ارکیده استفاده کنید. این گل‌ها
+                      همواره نمادی از خلوص و شکوه بوده‌اند و به زیبایی سفره عقد
+                      شما می‌افزایند.
+                    </p>
+                    <span className="font-bold text-gold">
+                      2 - ترکیب رنگ‌های روشن برای تم‌های مدرن:
+                    </span>
+                    <br />
+                    <p>
+                      در مراسم‌هایی با تم مدرن، استفاده از گل‌هایی با رنگ‌های
+                      پررنگ و زنده مانند بنفش، صورتی و آبی می‌تواند فضای شاد و
+                      پرانرژی ایجاد کند. این رنگ‌ها به‌خصوص برای سفره عقد ساده و
+                      شیک مناسب هستند.
+                    </p>
+                    <br />
+                  </li>
+                  <div className="mx-auto flex flex-col items-center my-2 px-1.5 py-1.5 bg-white rounded-lg">
+                    <Image
+                      src="/assets/images/200.webp"
+                      width={2000}
+                      height={2000}
+                      alt="  سفره عقد شاتو ورسای"
+                      className="rounded-lg shadow-lg lg:max-h-[900px] mx-auto"
+                    />
+                  </div>
+
+                  <li className="text-regular  mb-3 mt-4">
+                    <span className="font-bold text-lg ml-2 ">
+                      ۲. انتخاب نوع گل‌ها بر اساس فصل برگزاری مراسم
+                    </span>
+                    <br />
+                    <span className="p-2">
+                      یکی دیگر از موارد مهم در انتخاب گل‌ها، فصل برگزاری عروسی
+                      است. برخی از گل‌ها به‌طور طبیعی در فصول خاصی در دسترس
+                      هستند و می‌توانند با توجه به فصل، هماهنگی بیشتری با مراسم
+                      ایجاد کنند.
+                    </span>
+                    <br />
+                    <span className="font-bold text-gold">
+                      1 - گل‌های بهاری:
+                    </span>
+                    <p>
+                      در مراسم‌های بهاری، گل‌های طبیعی و تازه مانند لاله، نرگس و
+                      رز بسیار پرطرفدار هستند. این گل‌ها با رنگ‌های روشن خود به
+                      سفره عقد حسی از تازگی و سرزندگی می‌بخشند.
+                    </p>
+                    <span className="font-bold text-gold">
+                      2 - گل‌های تابستانی و پاییزی:
+                    </span>
+                    <br />
+                    <p>
+                      برای مراسم‌هایی که در تابستان یا پاییز برگزار می‌شوند،
+                      گل‌هایی با رنگ‌های گرم مانند آفتابگردان و گل داوودی مناسب
+                      هستند. این گل‌ها به خوبی با تم‌های گرم و طبیعی هماهنگی
+                      دارند.
+                    </p>
+
+                    <br />
+                  </li>
+                  <div className="mx-auto flex flex-col items-center my-2 px-1.5 py-1.5 bg-white rounded-lg">
+                    <Image
+                      src="/assets/images/202.webp"
+                      width={2000}
+                      height={2000}
+                      alt="  سفره عقد"
+                      className="rounded-lg shadow-lg lg:max-h-[900px] mx-auto"
+                    />
+                  </div>
+
+                  <li className="text-regular  mb-3">
+                    <span className="font-bold text-lg ml-2">
+                      ۳. استفاده از گل‌های مصنوعی یا طبیعی: مزایا و معایب
+                    </span>
+                    <br />
+                    <span className="p-2">
+                      یکی از سوالات رایج عروس و دامادها در مورد تزیین سفره عقد،
+                      انتخاب بین گل‌های طبیعی و مصنوعی است. هر دو گزینه دارای
+                      مزایا و معایب خاص خود هستند.
+                    </span>
+                    <br />
+                    <span className="font-bold text-gold">
+                      1 - گل‌های طبیعی:
+                    </span>
+                    <p>
+                      زیبایی و عطر گل‌های طبیعی باعث می‌شود فضای مراسم بسیار
+                      دلپذیر شود. اما این گل‌ها به دلیل حساسیت به دما و زمان
+                      کوتاه عمر، ممکن است هزینه‌برتر و نگهداری‌شان دشوارتر باشد.
+                    </p>
+                    <span className="font-bold text-gold">
+                      2 - گل‌های مصنوعی:
+                    </span>
+                    <br />
+                    <p>
+                      برای افرادی که به دنبال راه‌حلی مقرون‌به‌صرفه و بدون دردسر
+                      هستند، گل‌های مصنوعی انتخاب خوبی هستند. این گل‌ها
+                      می‌توانند در هر فصلی و با هر رنگی تهیه شوند و ماندگاری
+                      بالاتری دارند.
+                    </p>
+
+                    <br />
+                  </li>
+
+                  <li className="text-regular  mb-3">
+                    <span className="font-bold text-lg ml-2">
+                      ۴. هماهنگی گل‌های سفره عقد با گل‌آرایی سالن
+                    </span>
+                    <br />
+                    <span className="p-2">
+                      برای داشتن یک مراسم هماهنگ و زیبا، لازم است که گل‌های سفره
+                      عقد با گل‌آرایی سالن و دسته‌گل عروس نیز تطابق داشته باشند.
+                      این هماهنگی به فضای کلی مراسم یکپارچگی می‌بخشد و
+                      نشان‌دهنده دقت در طراحی مراسم است.
+                    </span>
+                    <br />
+                    <span className="font-bold text-gold">
+                      1 - هماهنگی با دسته‌گل عروس:
+                    </span>
+                    <p>
+                      اگر عروس دسته‌گلی با گل‌های سفید و سبز دارد، می‌توان همان
+                      ترکیب رنگی را در گل‌های سفره عقد نیز استفاده کرد تا
+                      هماهنگی بصری بیشتری ایجاد شود.
+                    </p>
+                    <span className="font-bold text-gold">
+                      2 - هماهنگی با دکوراسیون سالن:
+                    </span>
+                    <br />
+                    <p>
+                      انتخاب گل‌هایی که با دکوراسیون کلی سالن مانند گل‌آرایی
+                      میزها و ورودی سالن هماهنگ باشند، باعث می‌شود که سفره عقد
+                      به عنوان بخشی از کل مراسم به خوبی دیده شود.
+                    </p>
+
+                    <br />
+                  </li>
+                  <div className="mx-auto flex flex-col items-center my-2 px-1.5 py-1.5 bg-white rounded-lg">
+                    <Image
+                      src="/assets/images/183.webp"
+                      width={4000}
+                      height={4000}
+                      alt="  تزیین سفره عقد"
+                      className="rounded-lg shadow-lg lg:max-h-[900px] mx-auto"
+                    />
+                  </div>
+                  <li className="text-regular  mb-3">
+                    <span className="font-bold text-lg ml-2">
+                      ۵. تزیین گل‌ها با سایر عناصر سفره عقد
+                    </span>
+                    <br />
+                    <span className="p-2">
+                      گل‌ها به تنهایی زیبایی زیادی دارند، اما ترکیب آن‌ها با
+                      سایر عناصر سفره عقد مانند شمع‌ها، آینه و ظروف کریستالی
+                      می‌تواند جذابیت بیشتری به چیدمان ببخشد.
+                    </span>
+                    <br />
+                    <span className="font-bold text-gold">
+                      1 - ترکیب گل‌ها با شمع‌ها و آینه‌ها:
+                    </span>
+                    <p>
+                      استفاده از شمع‌های سفید یا طلایی در کنار گل‌های سفید و
+                      نقره‌ای می‌تواند فضایی رمانتیک و زیبا در سفره عقد ایجاد
+                      کند.
+                    </p>
+                    <span className="font-bold text-gold">
+                      2 - گل‌آرایی سفره عقد کریستال:
+                    </span>
+                    <br />
+                    <p>
+                      برای سفره عقد کریستال و لاکچری، گل‌هایی با رنگ‌های خاص و
+                      شیک مانند صورتی کمرنگ یا نقره‌ای می‌توانند هماهنگی جذابی
+                      ایجاد کنند.
+                    </p>
+
+                    <br />
+                  </li>
+                  <li className="text-regular  mb-3">
+                    <span className="font-bold text-lg ml-2">
+                      ۶. ایده‌هایی برای تزیین گل‌ها در سفره عقد ساده و خانگی
+                    </span>
+                    <br />
+                    <span className="p-2">
+                      برای عروس و دامادهایی که به دنبال سفره عقد ساده و خانگی
+                      هستند، می‌توان از گل‌های طبیعی و ارزان‌قیمت یا حتی گل‌های
+                      باغچه‌ای برای تزئین استفاده کرد. همچنین، استفاده از
+                      گلدان‌های کوچک یا ظروف سنتی می‌تواند حس گرما و صمیمیت به
+                      مراسم ببخشد.
+                    </span>
+                    <br />
+                    <span className="font-bold text-gold">
+                      1 - گل‌آرایی ساده با گلدان‌های کوچک:
+                    </span>
+                    <p>
+                      اگر به دنبال یک چیدمان ساده و دلنشین هستید، استفاده از
+                      گلدان‌های کوچک با گل‌های سفید و صورتی می‌تواند فضای سفره
+                      عقد را بسیار دوست‌داشتنی کند.
+                    </p>
+                    <span className="font-bold text-gold">
+                      2 - تزئین خانگی با گل‌های باغچه‌ای:
+                    </span>
+                    <br />
+                    <p>
+                      برای سفره عقد خانگی می‌توان از گل‌های طبیعی باغچه مانند
+                      نرگس، رز و لاله استفاده کرد که علاوه بر مقرون‌به‌صرفه
+                      بودن، حس طبیعی و دلنشینی به مراسم می‌دهند.
+                    </p>
+
+                    <br />
+                  </li>
+                  <li className="text-regular  mb-3">
+                    <span className="font-bold text-lg ml-2">
+                      ۷. نگهداری و آماده‌سازی گل‌ها قبل از مراسم
+                    </span>
+                    <br />
+                    <span className="p-2">
+                      برای آنکه گل‌ها در بهترین حالت خود در روز مراسم باقی
+                      بمانند، باید نکات مهمی در مورد نگهداری آن‌ها رعایت شود.
+                      گل‌ها باید در جای خنک نگهداری شوند و بهتر است چند ساعت قبل
+                      از مراسم آن‌ها را آماده و در سفره عقد قرار دهید.
+                    </span>
+                    <br />
+                    <span className="font-bold text-gold">
+                      1 - نگهداری گل‌ها در روز قبل از مراسم:
+                    </span>
+                    <p>
+                      بهتر است گل‌های طبیعی را یک روز قبل از مراسم تهیه کنید و
+                      آن‌ها را در آب خنک قرار دهید تا طراوت خود را حفظ کنند.
+                    </p>
+                    <span className="font-bold text-gold">
+                      2 - تزئین نهایی گل‌ها در روز مراسم:
+                    </span>
+                    <br />
+                    <p>
+                      چند ساعت قبل از شروع مراسم، گل‌ها را به‌دقت بر روی سفره
+                      عقد بچینید تا تازه و زیبا به نظر برسند.
+                    </p>
+
+                    <br />
+                  </li>
+                  <div className="mx-auto flex flex-col items-center my-2 px-1.5 py-1.5 bg-white rounded-lg">
+                    <Image
+                      src="/assets/images/203.webp"
+                      width={2000}
+                      height={2000}
+                      alt="  سفره عقد"
+                      className="rounded-lg shadow-lg lg:max-h-[900px] mx-auto"
+                    />
+                  </div>
+                </ol>
+                <h3 className="font-bold text-2xl my-2">نتیجه‌گیری</h3>
+
+                <span className="p-2">
+                  هماهنگی گل‌های سفره عقد با تم عروسی می‌تواند تاثیر زیادی بر
+                  جذابیت و زیبایی مراسم داشته باشد. با انتخاب درست گل‌ها بر اساس
+                  رنگ، نوع و فصل، و همچنین ترکیب آن‌ها با دیگر عناصر سفره عقد،
+                  می‌توانید یک فضای زیبا و هماهنگ برای روز بزرگ خود خلق کنید.
+                </span>
+                <br />
+
+                <Link href="https://tour.panoee.net/Versay/65df91a47a73f2e85a4da72b">
+                  <button className="bg-gradient-to-l from-green to-black text-white hover-btn py-2 px-8 mx-auto rounded-full shadow-lg hover:shadow-2xl transition-transform transform hover:scale-105 mt-8 mb-2 text-xl">
+                    میتوانید برای دیدن فضای سه بعدی مجموعه کلیک کنید.
+                  </button>
+                </Link>
+              </div>
+              <hr />
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+export default Page;
