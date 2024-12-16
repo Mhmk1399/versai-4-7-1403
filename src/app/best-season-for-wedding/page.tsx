e232c2da19c93ee00095704885f003f8e78874fa
Hossein <141348426+Hossein137900@users.noreply.github.com>
2024-12-16 15:44:06 +0330

    Add files via upload

---
+++ src/app/best-season-for-wedding/page.tsx
@@ -0,0 +1,244 @@
+"use client";
+import React, { useEffect } from "react";
+import Image from "next/image";
+import Link from "next/link";
+
+const Page = () => {
+  useEffect(() => {
+    document.title = " بهترین فصل برگزاری مراسم عروسی و نکات کلیدی برای هر فصل";
+    const metaDescription = document.querySelector('meta[name="description"]');
+    if (metaDescription) {
+      metaDescription.setAttribute(
+        "content",
+        "بهترین فصل برای مراسم عروسی کاملاً به سلیقه شما و شرایطی که در نظر دارید بستگی دارد. هر فصل زیبایی‌ها و چالش‌های خود را دارد."
+      );
+    }
+  }, []);
+  return (
+    <>
+      <div className="bg-white grid mx-auto grid-cols-1 h-auto p-4 " dir="rtl">
+        <div className="px-4 mx-auto py-2 bg-gray-50 rounded-lg my-2 width-full grid grid-cols-1 gap-4">
+          <div className="col-span-2 mx-auto width-auto my-2 px-1.5 py-1.5 bg-white rounded-lg">
+            <div className="mx-auto flex flex-col justify-center items-center my-2 px-1.5 py-1.5 bg-white rounded-lg">
+              <Image
+                src="/assets/images/210.webp"
+                width={4000}
+                height={4000}
+                alt="بهترین فصل برگزاری مراسم عروسی"
+                className="rounded-lg shadow-lg h-full"
+              />
+            </div>
+            <div className="mx-auto width-auto my-2 px-1.5 py-1.5 bg-white rounded-lg text-center">
+              <h1
+                className="text-2xl font-bold mx-auto h1-blog-header"
+                id="contetTitle"
+              >
+                بهترین فصل برگزاری مراسم عروسی و نکات کلیدی برای هر فصل{" "}
+              </h1>
+
+              <p className="p-2">
+                هر فصلی از سال ویژگی‌ها و جذابیت‌های خاص خود را برای برگزاری
+                مراسم عروسی دارد. از طبیعت بهاری و زیبایی‌های تابستان گرفته تا
+                رنگ‌های پاییزی و آرامش زمستان، هر فصل می‌تواند مراسمی خاص و
+                به‌یادماندنی برای شما رقم بزند. در این مقاله به بررسی نکات مهمی
+                که باید برای برگزاری مراسم عروسی در هر فصل سال در نظر گرفته شود،
+                می‌پردازیم.
+              </p>
+
+              <Link href="/menue">
+                <button className="bg-gradient-to-l mb-6 from-green to-black text-white hover-btn py-2 px-8 mx-auto rounded-full shadow-lg hover:shadow-2xl transition-transform transform hover:scale-105 mt-8 text-xl">
+                  رزرو سالن عقد شاتو دو ورسای
+                </button>
+              </Link>
+              <div className="mx-auto width-auto my-2 px-1.5 py-1.5 bg-white  rounded-lg text-center">
+                <h2 className="text-1xl font-bold mx-auto h2-blog-header ">
+                  چه فصلی برای عروسی شما بهترین است؟ بررسی مزایا و معایب هر فصل
+                </h2>
+                <ol
+                  id="contentDiscription"
+                  className="text-regular mx-auto text-gray-700 mt-5 "
+                >
+                  <span className="font-extrabold h-14 bg-gradient-to-r from-amber-500 px-3 py-1  to-Cyan-950 rounded-full">
+                    عروسی در چه فصلی باشکوه‌تر می‌شود؟
+                  </span>
+
+                  <li className="text-regular  mt-3 ">
+                    <span className="font-bold mb-3 text-lg ml-2">
+                      ۱. برگزاری مراسم عروسی در بهار: فصل شکوفه‌ها و طراوت
+                    </span>
+                    <br />
+                    <span className="p-2">
+                      بهار به عنوان فصل شکوفایی طبیعت و آغاز تازگی، یکی از
+                      بهترین زمان‌ها برای برگزاری مراسم عروسی است. هوای معتدل و
+                      تازه، همراه با گل‌های رنگارنگ، فضایی رمانتیک و رویایی را
+                      برای عروسی‌ها فراهم می‌کند.
+                    </span>
+                    <br />
+                    <span className="font-bold text-gold">
+                      1 - مزایا و نکات مهم:
+                    </span>
+                    <p>
+                      هوای مطبوع و شکوفایی گل‌ها، بهترین شرایط برای برگزاری
+                      مراسم در باغ‌ها و فضاهای باز است. تزیینات طبیعی و استفاده
+                      از گل‌های بهاری مثل رز و لاله می‌تواند زیبایی خاصی به
+                      مراسم ببخشد.
+                    </p>
+                    <span className="font-bold text-gold">2 - نکات کلیدی:</span>
+                    <br />
+                    <p>
+                      ممکن است به دلیل تغییرات جوی، بارندگی‌هایی رخ دهد،
+                      بنابراین حتماً باید گزینه‌ای برای فضای سرپوشیده در نظر
+                      داشته باشید.
+                    </p>
+                    <br />
+                  </li>
+                  <div className="mx-auto flex flex-col items-center my-2 px-1.5 py-1.5 bg-white rounded-lg">
+                    <Image
+                      src="/assets/images/spring.webp"
+                      width={2000}
+                      height={2000}
+                      alt=" بهترین فصل برگزاری مراسم عروسی"
+                      className="rounded-lg shadow-lg lg:max-h-[900px] mx-auto"
+                    />
+                  </div>
+
+                  <li className="text-regular  mb-3 mt-4">
+                    <span className="font-bold text-lg ml-2 ">
+                      ۲. تابستان: فصل گرما و انرژی برای عروسی در فضای باز
+                    </span>
+                    <br />
+                    <span className="p-2">
+                      تابستان با روزهای بلند و هوای گرم، انتخاب بسیاری از زوج‌ها
+                      برای برگزاری مراسم عروسی است. این فصل به دلیل امکان
+                      برگزاری مراسم در اواخر روز و شب‌های تابستانی، جذابیت‌های
+                      خاصی دارد.
+                    </span>
+                    <br />
+                    <span className="font-bold text-gold">
+                      1 - مزایا و نکات مهم:
+                    </span>
+                    <p>
+                      برگزاری مراسم در ساعات غروب یا شب به دلیل دمای مناسب‌تر،
+                      تجربه‌ای دل‌پذیر را فراهم می‌کند. نورپردازی‌های خاص در باغ
+                      تالار نیز در این فصل می‌تواند به زیبایی مراسم اضافه کند.
+                    </p>
+                    <span className="font-bold text-gold">2 - نکات کلیدی:</span>
+                    <br />
+                    <p>
+                      برای جلوگیری از گرمای بیش از حد، بهتر است برای مهمانان
+                      تهویه مناسبی در نظر بگیرید و همچنین مراقب نور خورشید و
+                      دمای هوا باشید.
+                    </p>
+
+                    <br />
+                  </li>
+                  <div className="mx-auto flex flex-col items-center my-2 px-1.5 py-1.5 bg-white rounded-lg">
+                    <Image
+                      src="/assets/images/170.webp"
+                      width={2000}
+                      height={2000}
+                      alt="بهترین فصل برگزاری مراسم عروسی "
+                      className="rounded-lg shadow-lg lg:max-h-[900px] mx-auto"
+                    />
+                  </div>
+
+                  <li className="text-regular  mb-3">
+                    <span className="font-bold text-lg ml-2">
+                      ۳. پاییز: فصل رنگ‌ها و رمانتیسم برای عروسی
+                    </span>
+                    <br />
+                    <span className="p-2">
+                      پاییز با رنگ‌های گرم و طبیعت دلنشین خود، انتخابی
+                      فوق‌العاده برای برگزاری مراسم عروسی است. این فصل به دلیل
+                      آب و هوای معتدل و زیبایی‌های طبیعی‌اش، فضایی خاص و متفاوت
+                      برای مراسم فراهم می‌کند.
+                    </span>
+                    <br />
+                    <span className="font-bold text-gold">
+                      1 - مزایا و نکات مهم:
+                    </span>
+                    <p>
+                      رنگ‌های طلایی و نارنجی در پاییز به تزیینات عروسی جلوه‌ای
+                      طبیعی و جذاب می‌بخشند. شما می‌توانید از برگ‌های پاییزی در
+                      تزیینات استفاده کنید و فضایی رمانتیک برای مراسم خود ایجاد
+                      کنید.
+                    </p>
+                    <span className="font-bold text-gold">2 - نکات کلیدی:</span>
+                    <br />
+                    <p>
+                      در پاییز هوا ممکن است خنک‌تر باشد، بنابراین برای مراسم در
+                      فضای باز بهتر است تدابیر گرمایشی یا پتوهای کوچک برای
+                      مهمانان فراهم کنید.
+                    </p>
+
+                    <br />
+                  </li>
+
+                  <li className="text-regular  mb-3">
+                    <span className="font-bold text-lg ml-2">
+                      ۴. زمستان: جشن ازدواج در فضایی گرم و دلنشین
+                    </span>
+                    <br />
+                    <span className="p-2">
+                      اگر به دنبال مراسمی خاص و متفاوت هستید، زمستان می‌تواند یک
+                      گزینه عالی برای شما باشد. برف‌های سفید، نورپردازی گرم و
+                      فضای داخلی گرم و صمیمی، می‌تواند مراسم شما را به یک جشن
+                      خاص و رویایی تبدیل کند.
+                    </span>
+                    <br />
+                    <span className="font-bold text-gold">
+                      1 - مزایا و نکات مهم:
+                    </span>
+                    <p>
+                      در زمستان معمولاً تاریخ‌های عروسی کمتر تقاضا دارند،
+                      بنابراین می‌توانید انتخاب بیشتری در تاریخ و مکان داشته
+                      باشید. همچنین برگزاری مراسم در فضای داخلی با تزیینات
+                      زمستانی مثل شمع‌ها و کریستال‌ها جلوه‌ای لوکس به مراسم
+                      می‌دهد.
+                    </p>
+                    <span className="font-bold text-gold">2 - نکات کلیدی:</span>
+                    <br />
+                    <p>
+                      سرمای هوا ممکن است چالشی باشد، بنابراین حتماً مطمئن شوید
+                      که فضای داخلی گرم و راحت برای مهمانان فراهم است.
+                    </p>
+
+                    <br />
+                  </li>
+                  <div className="mx-auto flex flex-col items-center my-2 px-1.5 py-1.5 bg-white rounded-lg">
+                    <Image
+                      src="/assets/images/winter.webp"
+                      width={4000}
+                      height={4000}
+                      alt="  تزیین سفره عقد"
+                      className="rounded-lg shadow-lg lg:max-h-[900px] mx-auto"
+                    />
+                  </div>
+                </ol>
+                <h3 className="font-bold text-2xl my-2">نتیجه‌گیری</h3>
+
+                <span className="p-2">
+                  هر فصلی از سال دارای ویژگی‌های منحصر به فردی برای برگزاری
+                  مراسم عروسی است. با توجه به شرایط آب و هوایی و زیبایی‌های
+                  طبیعی هر فصل، شما می‌توانید بهترین زمان را برای جشن عروسی خود
+                  انتخاب کنید. مهم‌ترین نکته این است که با برنامه‌ریزی دقیق و در
+                  نظر گرفتن جزئیات، مراسمی به‌یادماندنی برای خود و مهمانانتان
+                  رقم بزنید.
+                </span>
+                <br />
+
+                <Link href="https://tour.panoee.net/Versay/65df91a47a73f2e85a4da72b">
+                  <button className="bg-gradient-to-l from-green to-black text-white hover-btn py-2 px-8 mx-auto rounded-full shadow-lg hover:shadow-2xl transition-transform transform hover:scale-105 mt-8 mb-2 text-xl">
+                    میتوانید برای دیدن فضای سه بعدی مجموعه کلیک کنید.
+                  </button>
+                </Link>
+              </div>
+              <hr />
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+export default Page;
