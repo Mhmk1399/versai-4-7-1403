1732022a1a464fe988b6728c26ef2bec58664cb8
Hossein <141348426+Hossein137900@users.noreply.github.com>
2024-12-21 15:28:43 +0330

    Update page.tsx

--- src/app/blogs/page.tsx
+++ src/app/blogs/page.tsx
@@ -65,6 +65,9 @@ const handleButtonClick20 = () => {
 const handleButtonClick21 = () => {
   window.location.href = "/best-season-for-wedding";
 };
+const handleButtonClick22 = () => {
+  window.location.href = "/holding-a-winter-wedding";
+};
 
 const Page = () => {
   useEffect(() => {
@@ -1254,6 +1257,60 @@ const Page = () => {
               <span className="block text-xs ">
                 بهترین فصل برگزاری مراسم عروسی
               </span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div
+        className="p-1 flex flex-wrap items-center justify-center"
+        onClick={handleButtonClick22}
+      >
+        <div className="flex-shrink-0 m-6 relative overflow-hidden bg-green rounded-lg max-w-xs shadow-lg">
+          <svg
+            className="absolute bottom-0 left-0 mb-8"
+            viewBox="0 0 375 283"
+            fill="none"
+            style={{ transform: "scale(1.5)", opacity: 0.1 }}
+          >
+            <rect
+              x="159.52"
+              y="175"
+              width="152"
+              height="152"
+              rx="8"
+              transform="rotate(-45 159.52 175)"
+              fill="white"
+            />
+            <rect
+              y="107.48"
+              width="152"
+              height="152"
+              rx="8"
+              transform="rotate(-45 0 107.48)"
+              fill="white"
+            />
+          </svg>
+          <div className="relative pt-10 px-10 flex items-center justify-center">
+            <div
+              className="block absolute w-48 h-48 bottom-0 left-0 -mb-24 ml-3"
+              style={{
+                background: "radial-gradient(black, transparent 60%)",
+                transform: "rotate3d(0, 0, 1, 20deg) scale3d(1, 0.6, 1)",
+                opacity: 0.2,
+              }}
+            ></div>
+            <Image
+              className="relative w-40"
+              src="/logo.webp"
+              alt=""
+              width={4000}
+              height={4000}
+            />
+          </div>
+          <div className="relative text-white px-6 pb-6 mt-6">
+            <span className="block opacity-75 -mb-1"></span>
+            <div className="flex justify-center">
+              <span className="block text-xs ">برگزاری عروسی زمستانی </span>
             </div>
           </div>
         </div>
