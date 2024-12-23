5dd313812fbc05e71e26481deae6ff98aac3608c
Hossein <141348426+Hossein137900@users.noreply.github.com>
2024-12-23 14:04:50 +0330

    Add files via upload

---
+++ src/app/autumn-wedding/page.tsx
@@ -0,0 +1,308 @@
+"use client";
+import React, { useEffect } from "react";
+import Image from "next/image";
+import Link from "next/link";
+
+const Page = () => {
+  useEffect(() => {
+    document.title = " برگزاری عروسی پاییزی: ایده‌هایی برای تم و رنگ‌بندی";
+    const metaDescription = document.querySelector('meta[name="description"]');
+    if (metaDescription) {
+      metaDescription.setAttribute(
+        "content",
+        "جشن پاییزی خود را به یک خاطره ماندگار تبدیل کنید! با تم‌ها و ترکیب رنگ‌های بی‌نظیر پاییزی ، جشن شما متفاوت و شگفت‌انگیز خواهد بود."
+      );
+    }
+  }, []);
+  return (
+    <>
+      <div className="bg-white grid mx-auto grid-cols-1 h-auto p-4 " dir="rtl">
+        <div className="px-4 mx-auto py-2 bg-gray-50 rounded-lg my-2 width-full grid grid-cols-1 gap-4">
+          <div className="col-span-2 mx-auto width-auto my-2 px-1.5 py-1.5 bg-white rounded-lg">
+            <div className="mx-auto flex flex-col justify-center items-center my-2 px-1.5 py-1.5 bg-white rounded-lg">
+              <Image
+                src="/assets/images/photo_2024-11-16_14-00-54.webp"
+                width={4000}
+                height={4000}
+                alt="  برگزاری عروسی پاییزی"
+                className="rounded-lg shadow-lg h-full"
+              />
+            </div>
+            <div className="mx-auto width-auto my-2 px-1.5 py-1.5 bg-white rounded-lg text-center">
+              <h1
+                className="text-2xl font-bold mx-auto h1-blog-header"
+                id="contetTitle"
+              >
+                برگزاری عروسی پاییزی: ایده‌هایی برای تم و رنگ‌بندی
+              </h1>
+
+              <p className="p-2">
+                پاییز یکی از زیباترین فصل‌ها برای برگزاری مراسم عروسی است. این
+                فصل با رنگ‌های گرم و دلنشین، فرصت‌های بسیاری برای خلق جشنی
+                رویایی فراهم می‌کند. اگر در نظر دارید که عروسی‌تان را در این فصل
+                برگزار کنید، در این مقاله به شما ایده‌هایی برای انتخاب تم،
+                رنگ‌بندی، دکوراسیون، و حتی جزئیات خاص مانند دسته‌گل و کیک عروسی
+                پاییزی خواهیم داد. همچنین نکات الهام‌بخشی در مورد متن کارت عروسی
+                پاییزی، میوه‌های پاییزی، و موارد دیگر را بررسی خواهیم کرد.
+              </p>
+
+              <Link href="/menue">
+                <button className="bg-gradient-to-l mb-6 from-green to-black text-white hover-btn py-2 px-8 mx-auto rounded-full shadow-lg hover:shadow-2xl transition-transform transform hover:scale-105 mt-8 text-xl">
+                  رزرو سالن عقد شاتو دو ورسای
+                </button>
+              </Link>
+              <div className="mx-auto width-auto my-2 px-1.5 py-1.5 bg-white  rounded-lg text-center">
+                <h2 className="text-1xl font-bold mx-auto h2-blog-header ">
+                  جادوی پاییز در جشن شما: ایده‌هایی شگفت‌انگیز برای عروسی
+                  پاییزی!
+                </h2>
+                <ol
+                  id="contentDiscription"
+                  className="text-regular mx-auto text-gray-700 mt-5 "
+                >
+                  <span className="font-extrabold h-14 bg-gradient-to-r from-amber-500 px-3 py-1  to-Cyan-950 rounded-full">
+                    عشق در هوای پاییز
+                  </span>
+
+                  <li className="text-regular  mt-3 ">
+                    <span className="font-bold mb-3 text-lg ml-2">
+                      ۱. رنگ‌بندی عروسی پاییزی: از طبیعت الهام بگیرید
+                    </span>
+                    <br />
+                    <span className="p-2">
+                      رنگ‌های پاییز ترکیبی از گرمی و زیبایی طبیعت را به نمایش
+                      می‌گذارند. برای عروسی پاییزی، رنگ‌هایی مانند نارنجی، زرد،
+                      قهوه‌ای، قرمز، و حتی طلایی می‌توانند حال و هوای خاصی به
+                      مراسم ببخشند. همچنین ترکیب این رنگ‌ها با رنگ‌های خنثی
+                      مانند کرم، بژ و سفید جلوه‌ای شیک و متفاوت ایجاد می‌کند.
+                    </span>
+                    <br />
+                    <span className="mb-3 text-base">
+                      <strong className="ml-2 text-gold">
+                        رنگ‌های اصلی پیشنهادی:
+                      </strong>
+                      قهوه‌ای شکلاتی، قرمز تیره، نارنجی گرم، طلایی براق.
+                    </span>
+                    <br />
+                    <span className="mb-3 text-base">
+                      <strong className="ml-2 text-gold">رنگ‌های مکمل:</strong>
+                      سبز زمردی، بژ، خاکستری ملایم، سفید یخی.{" "}
+                    </span>
+                    <br />
+                    <br />
+                    <p>
+                      تم‌های پاییزی اغلب بر پایه رنگ‌های گرم طبیعی هستند که نه
+                      تنها در دکوراسیون، بلکه در لباس‌ها و لوازم جانبی نیز به
+                      کار گرفته می‌شوند. به عنوان مثال، می‌توانید از شمع‌های
+                      طلایی و نارنجی برای تزئین میزها استفاده کنید یا سفره‌هایی
+                      با طرح‌های برگ‌های پاییزی پهن کنید.
+                    </p>
+                    <br />
+                  </li>
+                  <div className="mx-auto flex flex-col items-center my-2 px-1.5 py-1.5 bg-white rounded-lg">
+                    <Image
+                      src="/assets/images/170.webp"
+                      width={2000}
+                      height={2000}
+                      alt="  برگزاری عروسی پاییزی"
+                      className="rounded-lg shadow-lg lg:max-h-[900px] mx-auto"
+                    />
+                  </div>
+
+                  <li className="text-regular  mb-3 mt-4">
+                    <span className="font-bold text-lg ml-2 ">
+                      ۲. ایده‌های خاص برای دکوراسیون پاییزی
+                    </span>
+                    <br />
+                    <span className="p-2">
+                      دکوراسیون مراسم عروسی پاییزی می‌تواند از عناصر طبیعی مانند
+                      برگ‌های خشک، کدوهای تنبل، شاخه‌های درختان و شمع‌های گرم و
+                      روشن بهره‌مند شود. این المان‌ها حس گرمی و صمیمیت را به
+                      فضای مراسم می‌آورند.
+                    </span>
+                    <br />
+                    <span className="mb-3 text-base">
+                      <strong className="ml-2 text-gold">
+                        تزیینات با برگ‌های پاییزی:{" "}
+                      </strong>
+                      استفاده از برگ‌های طلایی و قرمز بر روی میزها یا در طراحی
+                      ورودی سالن.{" "}
+                    </span>
+                    <br />
+                    <span className="mb-3 text-base">
+                      <strong className="ml-2 text-gold">
+                        شمع‌ها و نورپردازی ملایم:
+                      </strong>
+                      شمع‌های بزرگ در کنار میزها یا راهروها برای ایجاد حس گرم و
+                      دلپذیر پاییزی.{" "}
+                    </span>
+                    <br />
+                    <span className="mb-3 text-base">
+                      <strong className="ml-2 text-gold">
+                        المان‌های طبیعی:{" "}
+                      </strong>
+                      استفاده از کدوهای تزئینی، گل‌های خشک و شاخه‌های طبیعی در
+                      تزیینات، فضای شما را به یک پاییز واقعی تبدیل می‌کند.
+                    </span>
+                    <br />
+                  </li>
+                  <div className="mx-auto flex flex-col items-center my-2 px-1.5 py-1.5 bg-white rounded-lg">
+                    <Image
+                      src="/assets/images/View-of-table-and-chair-arrangement-in-the-garden-hall-of-Chateau-de-Versailles-luxury-hall.webp"
+                      width={2000}
+                      height={2000}
+                      alt="برگزاری عروسی پاییزی  "
+                      className="rounded-lg shadow-lg lg:max-h-[900px] mx-auto"
+                    />
+                  </div>
+
+                  <li className="text-regular  mb-3">
+                    <span className="font-bold text-lg ml-2">
+                      ۳. دسته گل و کیک عروسی پاییزی: جزئیات الهام‌بخش
+                    </span>
+                    <br />
+                    <span className="p-2">
+                      دسته گل و کیک عروسی پاییزی می‌تواند ترکیبی از گل‌های گرم و
+                      فصلی و تزیینات خاص باشد که حال و هوای پاییز را به مهمانان
+                      القا کند. استفاده از گل‌های فصلی و میوه‌های پاییزی در
+                      تزیین کیک و دسته گل می‌تواند زیبایی خاصی به مراسم ببخشد.
+                    </span>
+                    <br />
+                    <span className="mb-3 text-base">
+                      <strong className="ml-2 text-gold">
+                        دسته گل عروسی پاییزی:{" "}
+                      </strong>
+                      گل‌هایی مانند گل داوودی، رز قرمز، گل آفتابگردان و حتی
+                      برگ‌های پاییزی می‌توانند ترکیبی زیبا برای دسته‌گل عروس
+                      باشند. استفاده از نوارهایی به رنگ قهوه‌ای یا طلایی نیز
+                      دسته گل را تکمیل می‌کند.{" "}
+                    </span>
+                    <br />
+                    <span className="mb-3 text-base">
+                      <strong className="ml-2 text-gold">
+                        کیک عروسی پاییزی:
+                      </strong>
+                      کیک‌های پاییزی معمولاً با رنگ‌های گرم و تزیینات طبیعی
+                      مانند برگ‌های شکری، میوه‌های فصلی مثل انجیر و سیب و حتی
+                      چوب‌های دارچین تزئین می‌شوند.{" "}
+                    </span>
+
+                    <br />
+                  </li>
+
+                  <li className="text-regular  mb-3">
+                    <span className="font-bold text-lg ml-2">
+                      ۴. متن و کارت دعوت عروسی پاییزی: شعر و حس رمانتیک
+                    </span>
+                    <br />
+                    <span className="p-2">
+                      برای کارت عروسی پاییزی، می‌توانید از شعرهای مرتبط با پاییز
+                      استفاده کنید تا حس رمانتیک و خاص این فصل به مهمانان منتقل
+                      شود. متن کارت عروسی پاییزی می‌تواند الهام گرفته از طبیعت و
+                      رنگ‌های پاییز باشد و دعوتی شاعرانه به مراسم شما ارائه دهد.
+                    </span>
+                    <br />
+                    <span className="mb-3 text-base">
+                      <strong className="ml-2 text-gold">
+                        متن پیشنهادی کارت عروسی پاییزی:{" "}
+                      </strong>
+                      پاییز آمد و با خود شور و شوقی تازه آورد، ما شما را به جشن
+                      پیوند زندگیمان در این فصل رویایی دعوت می‌کنیم.
+                    </span>
+                    <br />
+                    <span className="mb-3 text-base">
+                      <strong className="ml-2 text-gold">
+                        شعر کارت عروسی پاییزی:{" "}
+                      </strong>
+                      در دل پاییز، عشق ما چون برگ‌های طلایی شکوفاست. بیایید و در
+                      این روز زیبا، با ما همراه شوید.
+                    </span>
+                    <br />
+                    <br />
+                    <p>
+                      کارت‌های عروسی می‌توانند با طرح‌های برگ‌های پاییزی،
+                      شاخه‌های درختان یا حتی ترکیب رنگ‌های گرم فصلی طراحی شوند.
+                      به این ترتیب، حس و حال فصل پاییز به مخاطبان منتقل خواهد
+                      شد.
+                    </p>
+
+                    <br />
+                  </li>
+                  <div className="mx-auto flex flex-col items-center my-2 px-1.5 py-1.5 bg-white rounded-lg">
+                    <Image
+                      src="/assets/images/Luxurious-hall-and-garden-hall-of-Chateau-de-Versailles5.webp"
+                      width={4000}
+                      height={4000}
+                      alt="  برگزاری عروسی پاییز "
+                      className="rounded-lg shadow-lg lg:max-h-[900px] mx-auto"
+                    />
+                  </div>
+                  <li className="text-regular  mb-3">
+                    <span className="font-bold text-lg ml-2">
+                      ۵. میوه‌های پاییزی برای عروسی: زیبایی در سادگی
+                    </span>
+                    <br />
+                    <span className="p-2">
+                      میوه‌های فصل پاییز از جمله مهم‌ترین عناصر طبیعی هستند که
+                      می‌توانند در تزیینات عروسی به کار گرفته شوند. میوه‌هایی
+                      مانند سیب، انجیر، انگور، و انار نه تنها زیبا هستند، بلکه
+                      به مهمانان شما طعمی از طبیعت پاییز ارائه می‌دهند.
+                    </span>
+                    <br />
+                    <span className="mb-3 text-base">
+                      <strong className="ml-2 text-gold">
+                        میوه‌های مناسب برای عروسی پاییزی:{" "}
+                      </strong>
+                      سیب‌های قرمز، انار، انگور، انجیر و گلابی. این میوه‌ها
+                      می‌توانند در سبدهای میوه یا به عنوان دکور روی میزها قرار
+                      گیرند.
+                    </span>
+                    <br />
+                    <span className="mb-3 text-base">
+                      <strong className="ml-2 text-gold">
+                        ایده‌های تزیینی با میوه‌ها:{" "}
+                      </strong>
+                      قرار دادن سبدهای میوه پاییزی در میان میزهای مهمانان،
+                      استفاده از میوه‌ها برای تزیین میز دسر و حتی قرار دادن
+                      آن‌ها بر روی کیک عروسی.
+                    </span>
+                    <br />
+
+                    <br />
+                    <p>
+                      این میوه‌ها نه تنها رنگ و جلوه خاصی به عروسی شما می‌بخشند،
+                      بلکه نمادی از فراوانی و برکت هستند و حس جشن و شادمانی
+                      پاییزی را تقویت می‌کنند.
+                    </p>
+
+                    <br />
+                  </li>
+                </ol>
+                <hr />
+                <br />
+                <h3 className="font-bold text-2xl my-2">نتیجه‌گیری</h3>
+
+                <span className="p-2">
+                  عروسی پاییزی فرصتی استثنایی برای خلق فضایی گرم، دلنشین و زیبا
+                  است. از رنگ‌های زنده و گرم پاییز گرفته تا دکوراسیون با الهام
+                  از طبیعت و میوه‌های فصل، همه می‌توانند به مراسم شما زیبایی و
+                  جذابیت بیشتری ببخشند. با انتخاب تم و جزئیات مناسب، شما
+                  می‌توانید یک عروسی رویایی در این فصل خاطره‌انگیز داشته باشید.
+                </span>
+                <br />
+
+                <Link href="https://tour.panoee.net/Versay/65df91a47a73f2e85a4da72b">
+                  <button className="bg-gradient-to-l from-green to-black text-white hover-btn py-2 px-8 mx-auto rounded-full shadow-lg hover:shadow-2xl transition-transform transform hover:scale-105 mt-8 mb-2 text-xl">
+                    میتوانید برای دیدن فضای سه بعدی مجموعه کلیک کنید.
+                  </button>
+                </Link>
+              </div>
+              <hr />
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+export default Page;
