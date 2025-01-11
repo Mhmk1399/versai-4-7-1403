131db145c5bdbad8278dce24b6e50a972f764dff
Hossein <141348426+Hossein137900@users.noreply.github.com>
2025-01-11 15:57:56 +0330

    Add files via upload

---
+++ src/app/cheap-wedding-with-high-quality/page.tsx
@@ -0,0 +1,225 @@
+"use client";
+import React, { useEffect } from "react";
+import Image from "next/image";
+import Link from "next/link";
+
+const Page = () => {
+  useEffect(() => {
+    document.title =
+      "تشریفات عروسی ارزان با کیفیت بالا: نکاتی برای برگزاری مجالس به‌یادماندنی";
+    const metaDescription = document.querySelector('meta[name="description"]');
+    if (metaDescription) {
+      metaDescription.setAttribute(
+        "content",
+        "برنامه‌ریزی دقیق و مدیریت زمان: با برنامه‌ریزی مناسب و هماهنگی با تیم‌های خدماتی، می‌توان از هزینه‌های اضافی جلوگیری کرد و مراسمی به‌یادماندنی برگزار نمود."
+      );
+    }
+  }, []);
+  return (
+    <>
+      <div className="bg-white grid mx-auto grid-cols-1 h-auto p-4 " dir="rtl">
+        <div className="px-4 mx-auto py-2 bg-gray-50 rounded-lg my-2 width-full grid grid-cols-1 gap-4">
+          <div className="col-span-2 mx-auto width-auto my-2 px-1.5 py-1.5 bg-white rounded-lg">
+            <div className="mx-auto flex flex-col justify-center items-center my-2 px-1.5 py-1.5 bg-white rounded-lg">
+              <Image
+                src="/assets/images/155.webp"
+                width={4000}
+                height={4000}
+                alt="تشریفات عروسی ارزان "
+                className="rounded-lg shadow-lg h-full"
+              />
+            </div>
+            <div className="mx-auto width-auto my-2 px-1.5 py-1.5 bg-white rounded-lg text-center">
+              <h1
+                className="text-2xl font-bold mx-auto h1-blog-header"
+                id="contetTitle"
+              >
+                تشریفات عروسی ارزان با کیفیت بالا: نکاتی برای برگزاری مجالس
+                به‌یادماندنی
+              </h1>
+
+              <p className="p-2">
+                وقتی می‌خوای یه مراسم عروسی برگزار کنی، همیشه به این فکر می‌کنی
+                که باید خیلی هزینه کنی تا عروسیت شیک و مجلل باشه. ولی نگران
+                نباش! ما اینجا بهت نکات و ترفندهایی یاد می‌دیم که با یه بودجه
+                مناسب، عروسی‌ت رو به یادماندنی و با کیفیت برگزار کنی. توی این
+                مقاله، قدم به قدم بهت توضیح می‌دیم چطوری می‌تونی هم توی هزینه‌ها
+                صرفه‌جویی کنی و هم مراسمی داشته باشی که هیچ‌کس از یادش نره.
+              </p>
+
+              <Link href="/menue">
+                <button className="bg-gradient-to-l mb-6 from-green to-black text-white hover-btn py-2 px-8 mx-auto rounded-full shadow-lg hover:shadow-2xl transition-transform transform hover:scale-105 mt-8 text-xl">
+                  رزرو سالن عقد شاتو دو ورسای
+                </button>
+              </Link>
+              <div className="mx-auto width-auto my-2 px-1.5 py-1.5 bg-white  rounded-lg text-center">
+                <h2 className="text-1xl font-bold mx-auto h2-blog-header ">
+                  تشریفات مجالس باکیفیت و قیمت مناسب: چگونه ممکن است؟{" "}
+                </h2>
+
+                <ol
+                  id="contentDiscription"
+                  className="text-regular mx-auto text-gray-700 mt-5 "
+                >
+                  <span className="font-extrabold h-14 bg-gradient-to-r from-amber-500 px-3 py-1  to-Cyan-950 rounded-full">
+                    رازهای برگزاری عروسی شیک و ارزان{" "}
+                  </span>
+
+                  <li className="text-regular  mt-3 ">
+                    <br />
+                    <span className="p-2 font-bold mt-5 text-gold">
+                      ۱. اولویت‌بندی کارهای تشریفاتی
+                    </span>
+
+                    <p className="p-2">
+                      اولین قدم برای کاهش هزینه‌ها اینه که بدونی دقیقاً از
+                      مراسمت چی می‌خوای. مثلاً دوست داری تمرکز روی دکوراسیون
+                      باشه؟ یا شاید می‌خوای یه پذیرایی خاص داشته باشی؟ وقتی
+                      بتونی اولویت‌ها رو درست بچینی، راحت‌تر می‌تونی هزینه‌ها رو
+                      مدیریت کنی. برای این کار، اول یه لیست از تمام خدمات
+                      تشریفاتی که می‌خوای تهیه کن. بعد مشخص کن که کدوم بخش‌ها
+                      واقعاً مهم‌ترن. اگه دنبال یه منبع عالی برای پیدا کردن
+                      بهترین خدمات تشریفاتی هستی، سایت{" "}
+                      <Link
+                        className="text-blue-500 font-semibold"
+                        href="https://www.banktalar.com/"
+                      >
+                        بانک تالار
+                      </Link>{" "}
+                      رو فراموش نکن. اونجا می‌تونی خدمات مختلف تشریفاتی رو با
+                      قیمت‌های متنوع ببینی و بهترین‌ها رو انتخاب کنی.
+                    </p>
+                    <br />
+                    <span className="p-2 font-bold mt-5 text-gold">
+                      ۲. اجاره وسایل تشریفاتی با صرفه اقتصادی
+                    </span>
+
+                    <p className="p-2">
+                      یکی از بهترین راه‌ها برای کاهش هزینه‌ها، اجاره کردن وسایل
+                      تشریفاتی مثل میز و صندلی روستیکه. این وسایل، علاوه بر این
+                      که به مراسمت یه حس خیلی خاص و طبیعتی می‌ده، هزینه‌های
+                      کمتری هم دارن. برعکس وسایل لوکس، این نوع از دکوراسیون
+                      می‌تونه خیلی اقتصادی‌تر باشه و جلوه‌ای شیک به فضای مراسمت
+                      بده.
+                    </p>
+                    <br />
+                    <span className="p-2 font-bold mt-5 text-gold">
+                      ۳. انتخاب باغ برای مراسم
+                    </span>
+
+                    <p className="p-2">
+                      برگزاری مراسم عروسی در باغ یکی دیگه از گزینه‌های عالی برای
+                      کاهش هزینه‌هاست. توی باغ، خیلی از تزیینات داخلی لازم نیست،
+                      چون فضای طبیعی خودش به اندازه کافی زیباست. از نور طبیعی
+                      استفاده کن و دکوراسیون رو ساده نگه دار، این‌طوری هم پول
+                      بیشتری ذخیره می‌کنی و هم یه مراسم با حال‌وهوای خاص داری.
+                      اگه دنبال باغ‌تالارهایی با قیمت مناسب هستی، یه سری به سایت
+                      <Link
+                        className="text-blue-500 font-semibold mr-2"
+                        href="https://www.talarkadeh.com/"
+                      >
+                        تالارکده
+                      </Link>{" "}
+                      بزن. اونجا یه لیست از بهترین باغ‌تالارهای ایران رو می‌تونی
+                      پیدا کنی.
+                    </p>
+                    <br />
+                    <div className="mx-auto flex flex-col justify-center items-center my-2 px-1.5 py-1.5 bg-white rounded-lg">
+                      <Image
+                        src="/assets/images/The-English-garden-is-a-photography-location-of-the-Chateau-de-Versailles-hall-garden-inspired-by-the-Palace-of-Versailles.webp"
+                        width={4000}
+                        height={4000}
+                        alt="تشریفات عروسی ارزان "
+                        className="rounded-lg shadow-lg h-full"
+                      />
+                    </div>
+                    <br />
+                    <br />
+                    <span className="p-2 font-bold mt-5 text-gold">
+                      ۴. انتخاب یه تیم تشریفاتی با کیفیت و قیمت مناسب
+                    </span>
+                    <p>
+                      انتخاب تیم تشریفات هم یکی از مهم‌ترین بخش‌های برگزاری
+                      عروسیه. حواست باشه که همه چیز رو به یه شرکت گرون‌قیمت
+                      نسپاری. بعضی از شرکت‌های کوچکتر ولی با تجربه، می‌تونن
+                      خدماتی با کیفیت بالا و قیمت مناسب‌تر ارائه بدن. پس قبل از
+                      انتخاب، حتماً از چند جا قیمت بگیر و بهترین رو انتخاب کن.
+                    </p>
+                    <br />
+                    <span className="p-2 font-bold mt-5 text-gold">
+                      ۵. تزیینات ساده و زیبا
+                    </span>
+
+                    <p className="p-2">
+                      لازم نیست برای یه دکوراسیون شیک، حتماً هزینه‌های زیادی
+                      کنی. یه تزیین ساده اما شیک می‌تونه هم مهمان‌ها رو شگفت‌زده
+                      کنه و هم از نظر بودجه‌ای خیلی به نفعت باشه. استفاده از
+                      گل‌های طبیعی، نورپردازی ساده و اجاره وسایل ساده می‌تونه یه
+                      جلوه خاص به مراسم بده بدون این که هزینه زیادی داشته باشه.
+                    </p>
+                    <br />
+                    <div className="mx-auto flex flex-col justify-center items-center my-2 px-1.5 py-1.5 bg-white rounded-lg">
+                      <Image
+                        src="/assets/images/S__00448.webp"
+                        width={4000}
+                        height={4000}
+                        alt="تشریفات عروسی ارزان "
+                        className="rounded-lg shadow-lg h-full"
+                      />
+                    </div>
+                    <br />
+                    <br />
+                    <span className="p-2 font-bold mt-5 text-gold">
+                      ۶. منوی پذیرایی اقتصادی
+                    </span>
+
+                    <p className="p-2">
+                      پذیرایی یکی دیگه از بخش‌های مهم مراسمه که می‌تونه خیلی خرج
+                      برداره. اما می‌تونی منویی انتخاب کنی که هم قیمت مناسبی
+                      داشته باشه و هم مهمون‌ها ازش راضی باشن. به جای انتخاب
+                      غذاهای لوکس و گرون، سعی کن روی غذاهای با کیفیت ولی ساده‌تر
+                      تمرکز کنی. مهم اینه که غذاها خوشمزه باشن و از نظر تنوع خوب
+                      باشن.
+                    </p>
+                    <br />
+
+                    <div className="mx-auto flex flex-col justify-center items-center my-2 px-1.5 py-1.5 bg-white rounded-lg">
+                      <Image
+                        src="/assets/images/photo_2024-11-16_14-12-21.webp"
+                        width={4000}
+                        height={4000}
+                        alt="تشریفات عروسی ارزان "
+                        className="rounded-lg shadow-lg h-full"
+                      />
+                    </div>
+                  </li>
+                </ol>
+                <hr />
+                <br />
+                <br />
+                <Link href="https://tour.panoee.net/Versay/65df91a47a73f2e85a4da72b">
+                  <button className="bg-gradient-to-l from-green to-black text-white hover-btn py-2 px-8 mx-auto rounded-full shadow-lg hover:shadow-2xl transition-transform transform hover:scale-105 mt-8 mb-2 text-xl">
+                    میتوانید برای دیدن فضای سه بعدی مجموعه کلیک کنید.
+                  </button>
+                </Link>
+              </div>
+              <hr />
+              <h3 className="font-bold text-2xl my-2">نتیجه‌گیری</h3>
+
+              <p className="p-4">
+                همون‌طور که دیدی، برگزاری یه مراسم عروسی زیبا و به‌یادماندنی با
+                یه بودجه معقول اصلاً دور از دسترس نیست. فقط کافیه اولویت‌ها رو
+                درست تعیین کنی، از منابع اقتصادی مثل اجاره وسایل استفاده کنی و
+                بهترین تیم تشریفاتی رو با دقت انتخاب کنی. با دنبال کردن این
+                نکات، مطمئن باش که می‌تونی یه جشن خاطره‌انگیز و بی‌نقص داشته
+                باشی، بدون این که توی خرج و هزینه غرق بشی.
+              </p>
+              <br />
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+export default Page;
