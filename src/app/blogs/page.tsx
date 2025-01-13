e37a2d2500f3f5a913ea624df81ebec9b1abeaea
Hossein <141348426+Hossein137900@users.noreply.github.com>
2025-01-13 12:11:13 +0330

    Update page.tsx

--- src/app/blogs/page.tsx
+++ src/app/blogs/page.tsx
@@ -89,6 +89,9 @@ const handleButtonClick28 = () => {
 const handleButtonClick29 = () => {
   window.location.href = "/cheap-wedding-with-high-quality";
 };
+const handleButtonClick30 = () => {
+  window.location.href = "/best-ways-to-rent-ceremonies";
+};
 
 const Page = () => {
   useEffect(() => {
@@ -1717,6 +1720,62 @@ const Page = () => {
             <div className="flex justify-center">
               <span className="block text-xs ">
                 تشریفات عروسی ارزان با کیفیت بالا
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div
+        className="p-1 flex flex-wrap items-center justify-center"
+        onClick={handleButtonClick30}
+      >
+        <div className="flex-shrink-0 m-6 relative overflow-hidden bg-green rounded-lg max-w-xs shadow-lg">
+          <svg
+            className="absolute bottom-0 left-0 mb-8"
+            viewBox="0 0 375 283"
+            fill="none"
+            style={{ transform: "scale(1.5)", opacity: 0.1 }}
+          >
+            <rect
+              x="159.52"
+              y="175"
+              width="152"
+              height="152"
+              rx="8"
+              transform="rotate(-45 159.52 175)"
+              fill="white"
+            />
+            <rect
+              y="107.48"
+              width="152"
+              height="152"
+              rx="8"
+              transform="rotate(-45 0 107.48)"
+              fill="white"
+            />
+          </svg>
+          <div className="relative pt-10 px-10 flex items-center justify-center">
+            <div
+              className="block absolute w-48 h-48 bottom-0 left-0 -mb-24 ml-3"
+              style={{
+                background: "radial-gradient(black, transparent 60%)",
+                transform: "rotate3d(0, 0, 1, 20deg) scale3d(1, 0.6, 1)",
+                opacity: 0.2,
+              }}
+            ></div>
+            <Image
+              className="relative w-40"
+              src="/logo.webp"
+              alt=""
+              width={4000}
+              height={4000}
+            />
+          </div>
+          <div className="relative text-white px-6 pb-6 mt-6">
+            <span className="block opacity-75 -mb-1"></span>
+            <div className="flex justify-center">
+              <span className="block text-xs ">
+                بهترین راه‌های اجاره تشریفات مجالس{" "}
               </span>
             </div>
           </div>
