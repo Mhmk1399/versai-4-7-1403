f97398346a7f31d9c1d4cb8986c0581609e5f915
Hossein <141348426+SHEKAF1234@users.noreply.github.com>
2025-04-16 10:49:04 +0330

    weblog44

--- src/app/blogs/page.tsx
+++ src/app/blogs/page.tsx
@@ -132,6 +132,9 @@ const handleButtonClick42 = () => {
 const handleButtonClick43 = () => {
   window.location.href = "/the-best-wedding-hall-in-west-of-tehran";
 };
+const handleButtonClick44 = () => {
+  window.location.href = "/the-best-trends-in-wedding-hall-decoration";
+};
 
 const Page = () => {
   const blogData = {
@@ -2560,6 +2563,62 @@ const Page = () => {
               <div className="flex justify-center">
                 <span className="block text-xs ">
                   بهترین تالار عروسی در غرب تهران{" "}
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div
+          className="p-1 flex flex-wrap items-center justify-center"
+          onClick={handleButtonClick44}
+        >
+          <div className="flex-shrink-0 m-6 relative overflow-hidden bg-green rounded-lg max-w-xs shadow-lg">
+            <svg
+              className="absolute bottom-0 left-0 mb-8"
+              viewBox="0 0 375 283"
+              fill="none"
+              style={{ transform: "scale(1.5)", opacity: 0.1 }}
+            >
+              <rect
+                x="159.52"
+                y="175"
+                width="152"
+                height="152"
+                rx="8"
+                transform="rotate(-45 159.52 175)"
+                fill="white"
+              />
+              <rect
+                y="107.48"
+                width="152"
+                height="152"
+                rx="8"
+                transform="rotate(-45 0 107.48)"
+                fill="white"
+              />
+            </svg>
+            <div className="relative pt-10 px-10 flex items-center justify-center">
+              <div
+                className="block absolute w-48 h-48 bottom-0 left-0 -mb-24 ml-3"
+                style={{
+                  background: "radial-gradient(black, transparent 60%)",
+                  transform: "rotate3d(0, 0, 1, 20deg) scale3d(1, 0.6, 1)",
+                  opacity: 0.2,
+                }}
+              ></div>
+              <Image
+                className="relative w-40"
+                src="/logo.webp"
+                alt="blog"
+                width={4000}
+                height={4000}
+              />
+            </div>
+            <div className="relative text-white px-6 pb-6 mt-6">
+              <span className="block opacity-75 -mb-1"></span>
+              <div className="flex justify-center">
+                <span className="block text-xs ">
+                  جدیدترین ترندهای دکوراسیون تالار عروسی
                 </span>
               </div>
             </div>
