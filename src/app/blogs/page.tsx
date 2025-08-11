1024d973019ffa93bb7003d37e8ebbb9301f2507
wolfix1234 <130220073+wolfix1234@users.noreply.github.com>
2025-08-11 11:48:56 +0300

    78

--- src/app/blogs/page.tsx
+++ src/app/blogs/page.tsx
@@ -270,6 +270,12 @@ const handleButtonClick77 = () => {
   window.location.href = "/build-dream-life-versai";
 };
 
+const handleButtonClick78 = () => {
+  window.location.href = "/luxury-or-budget-wedding";
+};
+
+
+
 const Page = () => {
   const blogData = {
     title: "وبلاگ شاتو دو ورسای | مقالات تخصصی درباره برگزاری مراسم عروسی",
@@ -4743,6 +4749,67 @@ const Page = () => {
           </div>
         </div>
 
+
+
+
+
+
+<div
+          className="p-1 flex flex-wrap items-center justify-center"
+          onClick={handleButtonClick78}
+        >
+          <div className="flex-shrink-0 m-6 relative overflow-hidden bg-green rounded-lg max-w-xs shadow-lg">
+            <svg
+              className="absolute bottom-0 left-0 mb-8"
+              viewBox="0 0 375 283"
+              fill="none"
+              style={{ transform: "scale(1.5)", opacity: 0.1 }}
+            >
+              <rect
+                x="159.52"
+                y="175"
+                width="152"
+                height="152"
+                rx="8"
+                transform="rotate(-45 159.52 175)"
+                fill="white"
+              />
+              <rect
+                y="107.48"
+                width="152"
+                height="152"
+                rx="8"
+                transform="rotate(-45 0 107.48)"
+                fill="white"
+              />
+            </svg>
+            <div className="relative pt-10 px-10 flex items-center justify-center">
+              <div
+                className="block absolute w-48 h-48 bottom-0 left-0 -mb-24 ml-3"
+                style={{
+                  background: "radial-gradient(black, transparent 60%)",
+                  transform: "rotate3d(0, 0, 1, 20deg) scale3d(1, 0.6, 1)",
+                  opacity: 0.2,
+                }}
+              ></div>
+              <Image
+                className="relative w-40"
+                src="/logo.webp"
+                alt="blog"
+                width={4000}
+                height={4000}
+              />
+            </div>
+            <div className="relative text-white px-6 pb-6 mt-6">
+              <span className="block opacity-75 -mb-1"></span>
+              <div className="flex justify-center">
+                <span className="block text-xs ">
+ عروسی مجلل یا کم‌هزینه
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
 
 
       </div>
